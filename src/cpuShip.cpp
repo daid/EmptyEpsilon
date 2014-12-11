0e54029e678710fe3bf1debd69ffea7909e65039
daid <daid303@gmail.com>
2014-12-11 08:16:07 +0100

    Slightly adjust how defend target AI is implemented.

--- src/cpuShip.cpp
+++ src/cpuShip.cpp
@@ -208,7 +208,8 @@ void CpuShip::update(float delta)
                 sf::Vector2f target_position = order_target->getPosition();
                 pathPlanner.plan(getPosition(), target_position);
                 target_position = pathPlanner.route[0];
-                target_position += sf::vector2FromAngle(sf::vector2ToAngle(target_position - getPosition()) + 170.0f) * (1500.0f + order_target->getRadius());
+                float circle_distance = 2000.0f + order_target->getRadius() * 2.0 + getRadius() * 2.0;
+                target_position += sf::vector2FromAngle(sf::vector2ToAngle(target_position - getPosition()) + 170.0f) * circle_distance;
             }else{
                 orders = AI_Roaming;    //We pretty much lost our defending target, so just start roaming.
             }
