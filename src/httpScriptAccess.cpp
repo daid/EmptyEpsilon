f2c0b3eb20738ace9445e4a457c5935a23d31228
vslotman <vslotman@gmail.com>
2015-03-07 12:41:33 +0100

    Added LUA-interaction through URI parameters
    We now have a get_data.lua and set_data.lua URI-path which we can use to
    query and set game variables through LUA-code

--- src/httpScriptAccess.cpp
+++ src/httpScriptAccess.cpp
@@ -20,5 +20,85 @@ bool HttpScriptHandler::handleRequest(HttpRequest& request, HttpServerConnection
         script->destroy();
         return true;
     }
+    else if (request.path == "/get_data.lua")
+    {
+        if (!gameGlobalInfo)
+        {
+            connection->sendString("{\"ERROR\": \"No game\"}");
+            return true;
+        }
+
+        string luaCode;
+        string objectId = "getPlayerShip(-1)";
+        std::map<string, string>::iterator i;
+
+        i = request.parameters.find(sOBJECT);
+        if (i != request.parameters.end())
+        {
+            objectId = request.parameters[sOBJECT];
+            request.parameters.erase(i);
+        }
+
+        luaCode = "object = " + objectId + "\n" +
+               "if object == nil then return {error = \"No valid object\"} end\n" +
+               "return {";
+
+        for (i = request.parameters.begin(); i != request.parameters.end(); i++)
+        {
+            luaCode += i->first + " = object:" + i->second + ", ";
+        }   luaCode += "}";
+
+        P<ScriptObject> script = new ScriptObject();
+        script->setMaxRunCycles(100000);
+
+        string output;
+        if (!script->runCode(luaCode, output))
+            connection->sendString("{\"ERROR\": \"Script error\"}");
+        else
+            connection->sendString(output);
+        return true;
+    }
+    else if (request.path == "/set_data.lua")
+    {
+        if (!gameGlobalInfo)
+        {
+            connection->sendString("{\"ERROR\": \"No game\"}");
+            return true;
+        }
+
+        string luaCode;
+        string objectId = "getPlayerShip(-1)";
+        std::map<string, string>::iterator i;
+
+        i = request.parameters.find(sOBJECT);
+        if (i != request.parameters.end())
+        {
+            objectId = request.parameters[sOBJECT];
+            request.parameters.erase(i);
+        }
+
+        luaCode = "object = " + objectId + "\n" +
+               "if object == nil then return {error = \"No valid object\"} end\n";
+
+        for (i = request.parameters.begin(); i != request.parameters.end(); i++)
+        {
+            if (i->second == sFALSE)
+                luaCode += "object:" + i->first + ";\n";
+            else
+                luaCode += i->first + ":" + i->second + ";\n";
+        }
+
+        P<ScriptObject> script = new ScriptObject();
+        script->setMaxRunCycles(100000);
+
+        string output;
+        if (!script->runCode(luaCode, output))
+            connection->sendString("{\"ERROR\": \"Script error\"}");
+        else
+            connection->sendString(output);
+        return true;
+    }
+
+
     return false;
 }
