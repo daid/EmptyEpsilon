bc8bf0b0bbd68e39d46e5ad2ab1eaa83a97b3be7
daid <daid303@gmail.com>
2015-07-31 16:33:28 +0200

    Add artemis mission converter script.

---
+++ artemis_mission_convert_template.lua
@@ -0,0 +1,91 @@
+
+function init()
+    timers = {}
+    fleet = {}
+	temp_transmission_object = SpaceStation():setTemplate("Small Station"):setPosition(-1000000, -1000000)
+###START###
+end
+
+function update(delta)
+    for key, value in pairs(timers) do
+        timers[key] = timers[key] - delta
+    end
+###EVENTS###
+end
+
+--[[
+	Utility functions
+--]]
+function vectorFromAngle(angle, length)
+    return math.cos(angle / 180 * math.pi) * length, math.sin(angle / 180 * math.pi) * length
+end
+
+function ifOutsideBox(obj, x1, y1, x2, y2)
+	if obj == nil or not obj:isValid() then
+		return false
+	end
+	x, y = obj:getPosition()
+	if x >= x1 and x <= x2 and y >= y1 and y <= y2 then
+		return false
+	end
+	return true
+end
+
+function ifInsideBox(obj, x1, y1, x2, y2)
+	if obj == nil or not obj:isValid() then
+		return false
+	end
+	x, y = obj:getPosition()
+	if x >= x1 and x <= x2 and y >= y1 and y <= y2 then
+		return true
+	end
+	return false
+end
+
+function ifInsideSphere(obj, x1, y1, r)
+	if obj == nil or not obj:isValid() then
+		return false
+	end
+	x, y = obj:getPosition()
+	xd, yd = (x1 - x), (y1 - y)
+	if math.sqrt(xd * xd + yd * yd) < r then
+		return true
+	end
+	return false
+end
+
+function ifOutsideSphere(obj, x1, y1, r)
+	if obj == nil or not obj:isValid() then
+		return false
+	end
+	x, y = obj:getPosition()
+	xd, yd = (x1 - x), (y1 - y)
+	if math.sqrt(xd * xd + yd * yd) < r then
+		return false
+	end
+	return true
+end
+
+function ifdocked(obj)
+	-- TODO: Only checks the first player ship.
+	return getPlayerShip(-1):isDocked(obj)
+end
+
+function countFleet(fleetnr)
+	count = 0
+	if fleet[fleetnr] ~= nil then
+		for key, value in pairs(fleet[fleetnr]) do
+			if value:isValid() then
+				count = count + 1
+			end
+		end
+	end
+	return count
+end
+
+function distance(obj1, obj2)
+	x1, y1 = obj1:getPosition()
+	x2, y2 = obj2:getPosition()
+	xd, yd = (x1 - x2), (y1 - y2)
+	return math.sqrt(xd * xd + yd * yd)
+end
