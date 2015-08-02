7cfb3c9ac708ff1f83e38a1c3b2aa703b0dd9be4
daid <daid303@gmail.com>
2015-08-02 16:49:34 +0200

    Add artifact object which can be used in mission scripting.

---
+++ src/spaceObjects/artifact.h
@@ -0,0 +1,26 @@
+#ifndef ARTIFACT_H
+#define ARTIFACT_H
+
+#include "spaceObject.h"
+
+class Artifact : public SpaceObject, public Updatable
+{
+private:
+    string current_model_data_name;
+    string model_data_name;
+    bool allow_pickup;
+public:
+    Artifact();
+
+    virtual void update(float delta);
+
+    virtual void drawOnRadar(sf::RenderTarget& window, sf::Vector2f position, float scale, bool long_range);
+
+    virtual void collide(Collisionable* target);
+    
+    void setModel(string name);
+    void explode();
+    void allowPickup(bool allow);
+};
+
+#endif//ARTIFACT_H
