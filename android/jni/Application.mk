27bc4a7ae053c853fe23b98283a344a0e14bc649
daid <daid303@gmail.com>
2015-08-03 09:10:52 +0200

    Build android app for all architectures.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,7 +1,7 @@
 NDK_TOOLCHAIN_VERSION := 4.8
 APP_PLATFORM := android-9
 APP_STL := c++_shared
-APP_ABI := armeabi-v7a
+APP_ABI := all
 APP_MODULES := sfml-activity emptyepsilon
 
 APP_CFLAGS += -DVERSION_NUMBER=$(shell date +%Y%m%d)
