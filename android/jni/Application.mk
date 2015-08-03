a0a8e35b50acdee5f072da7931290e9ab35085ab
daid <daid303@gmail.com>
2015-08-03 09:16:00 +0200

    Apperently "all" also builds 64bit targets, which SFML does not supply libraries for. So only use the 32bit targets.

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,7 +1,7 @@
 NDK_TOOLCHAIN_VERSION := 4.8
 APP_PLATFORM := android-9
 APP_STL := c++_shared
-APP_ABI := all
+APP_ABI := armeabi armeabi-v7a x86 mips
 APP_MODULES := sfml-activity emptyepsilon
 
 APP_CFLAGS += -DVERSION_NUMBER=$(shell date +%Y%m%d)
