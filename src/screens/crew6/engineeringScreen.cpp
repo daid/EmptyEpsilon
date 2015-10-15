400881c240d354b87f229ad56bc8b421c65a7041
daid <daid303@gmail.com>
2015-10-15 20:19:38 +0200

    Oops on #54

--- src/screens/crew6/engineeringScreen.cpp
+++ src/screens/crew6/engineeringScreen.cpp
@@ -95,7 +95,7 @@ void EngineeringScreen::onDraw(sf::RenderTarget& window)
             hull_display->setColor(sf::Color::Red);
         else
             hull_display->setColor(sf::Color::White);
-        shields_display->setValue(string(my_spaceship->getFrontShieldPercentage()) + "% " + string(int(100 * my_spaceship->rear_shield / my_spaceship->rear_shield_max)) + "%");
+        shields_display->setValue(string(my_spaceship->getFrontShieldPercentage()) + "% " + string(my_spaceship->getRearShieldPercentage()) + "%");
         
         for(int n=0; n<SYS_COUNT; n++)
         {
