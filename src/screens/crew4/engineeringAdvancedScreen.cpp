061050a113e3f59fac98b19aa83908a0f4d808dc
John <interestingjohn@gmail.com>
2016-03-24 11:23:05 -0700

    Revert "Merge remote-tracking branch 'daid/master' into gh-pages"
    This reverts commit 92a4d33d118fcd355e5e3fd74449393aa52b12f6, reversing
    changes made to 63b04dfee2709492b8be7211644a6b6f97747927.

--- src/screens/crew4/engineeringAdvancedScreen.cpp
+++
@@ -1,9 +0,0 @@
-#include "engineeringAdvancedScreen.h"
-
-#include "screenComponents/shieldsEnableButton.h"
-
-EngineeringAdvancedScreen::EngineeringAdvancedScreen(GuiContainer* owner)
-: EngineeringScreen(owner)
-{
-    (new GuiShieldsEnableButton(this, "SHIELDS_ENABLE"))->setPosition(-20, -420, ABottomRight)->setSize(320, 50);
-}
