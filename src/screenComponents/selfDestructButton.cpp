e8c989a88a0b7aa64464e7847bef66e37260943e
daid <daid303@gmail.com>
2016-04-02 09:24:47 +0200

    Fix the self destruct button icon #161

--- src/screenComponents/selfDestructButton.cpp
+++ src/screenComponents/selfDestructButton.cpp
@@ -4,20 +4,19 @@
 GuiSelfDestructButton::GuiSelfDestructButton(GuiContainer* owner, string id)
 : GuiElement(owner, id)
 {
-    setIcon("gui/icons/self-destruct");
     activate_button = new GuiButton(this, id + "_ACTIVATE", "Self destruct", [this](){
         activate_button->hide();
         confirm_button->show();
         cancel_button->show();
     });
-    activate_button->setSize(GuiElement::GuiSizeMax, 50);
+    activate_button->setIcon("gui/icons/self-destruct")->setSize(GuiElement::GuiSizeMax, 50);
 
     confirm_button = new GuiButton(this, id + "_CONFIRM", "Confirm!", [this](){
         confirm_button->hide();
         if (my_spaceship)
             my_spaceship->commandActivateSelfDestruct();
     });
-    confirm_button->hide()->setPosition(0, 50, ATopLeft)->setSize(GuiElement::GuiSizeMax, 50);
+    confirm_button->setIcon("gui/icons/self-destruct")->hide()->setPosition(0, 50, ATopLeft)->setSize(GuiElement::GuiSizeMax, 50);
     cancel_button = new GuiButton(this, id + "_CANCEL", "Cancel", [this](){
         activate_button->show();
         confirm_button->hide();
@@ -25,7 +24,7 @@ GuiSelfDestructButton::GuiSelfDestructButton(GuiContainer* owner, string id)
         if (my_spaceship)
             my_spaceship->commandCancelSelfDestruct();
     });
-    cancel_button->hide()->setSize(GuiElement::GuiSizeMax, 50);
+    cancel_button->setIcon("gui/icons/self-destruct")->hide()->setSize(GuiElement::GuiSizeMax, 50);
 }
 
 void GuiSelfDestructButton::onDraw(sf::RenderTarget& window)
