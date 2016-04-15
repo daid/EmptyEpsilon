38bb9383bdb20a300b8539de305ad8165aa3f062
daid <daid303@gmail.com>
2016-04-15 18:18:12 +0200

    Measure the multiplayer server update tick, as this seems to be a significant factor in the total time spend per frame.

--- src/gui/debugRenderer.cpp
+++ src/gui/debugRenderer.cpp
@@ -40,26 +40,32 @@ void DebugRenderer::render(sf::RenderTarget& window)
             timing_graph_points.clear();
         timing_graph_points.push_back(engine->getEngineTiming());
         sf::VertexArray update_points(sf::LinesStrip, timing_graph_points.size());
+        sf::VertexArray server_update_points(sf::LinesStrip, timing_graph_points.size());
         sf::VertexArray collision_points(sf::LinesStrip, timing_graph_points.size());
         sf::VertexArray render_points(sf::LinesStrip, timing_graph_points.size());
         for(unsigned int n=0; n<timing_graph_points.size(); n++)
         {
             update_points[n].position.x = float(n);
             update_points[n].position.y = window.getView().getSize().y - timing_graph_points[n].update * 10000;
+            server_update_points[n].position.x = float(n);
+            server_update_points[n].position.y = window.getView().getSize().y - timing_graph_points[n].server_update * 10000;
             collision_points[n].position.x = float(n);
             collision_points[n].position.y = window.getView().getSize().y - (timing_graph_points[n].update + timing_graph_points[n].collision) * 10000;
             render_points[n].position.x = float(n);
             render_points[n].position.y = window.getView().getSize().y - (timing_graph_points[n].render + timing_graph_points[n].update + timing_graph_points[n].collision) * 10000;
             
             update_points[n].color = sf::Color::Red;
+            server_update_points[n].color = sf::Color::Yellow;
             collision_points[n].color = sf::Color::Cyan;
             render_points[n].color = sf::Color::Green;
         }
+        window.draw(server_update_points);
         window.draw(update_points);
         window.draw(collision_points);
         window.draw(render_points);
         
         sf::Text text_update("Update: " + string(timing_graph_points.back().update * 1000) + "ms", *main_font, 18);
+        sf::Text text_server_update("ServerUpdate: " + string(timing_graph_points.back().server_update * 1000) + "ms", *main_font, 18);
         sf::Text text_collision("Collision: " + string(timing_graph_points.back().collision * 1000) + "ms", *main_font, 18);
         sf::Text text_render("Render: " + string(timing_graph_points.back().render * 1000) + "ms", *main_font, 18);
 
@@ -70,13 +76,16 @@ void DebugRenderer::render(sf::RenderTarget& window)
         fps60_line[1].color = sf::Color(255, 255, 255, 128);
         window.draw(fps60_line);
         
-        text_update.setPosition(0, window.getView().getSize().y - 18 * 3 - 170);
+        text_update.setPosition(0, window.getView().getSize().y - 18 * 4 - 170);
+        text_server_update.setPosition(0, window.getView().getSize().y - 18 * 3 - 170);
         text_collision.setPosition(0, window.getView().getSize().y - 18 * 2 - 170);
         text_render.setPosition(0, window.getView().getSize().y - 18 - 170);
         text_update.setColor(sf::Color::Red);
+        text_server_update.setColor(sf::Color::Yellow);
         text_collision.setColor(sf::Color::Cyan);
         text_render.setColor(sf::Color::Green);
         window.draw(text_update);
+        window.draw(text_server_update);
         window.draw(text_collision);
         window.draw(text_render);
     }
