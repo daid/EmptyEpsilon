6dedbf20410a4a4492c3ce81a8b43e9fe4966476
Jaime van Kessel <nallath@gmail.com>
2016-05-16 15:12:18 +0200

    Updated descriptions

--- scripts/shipTemplates_Frigates.lua
+++ scripts/shipTemplates_Frigates.lua
@@ -1,16 +1,18 @@
 --[[                  Frigates
 Frigates are 1 size up from starfighters. They require a crew from 3 to 20 people.
-Think, Firefly, millennium falcon.
-They generally have 2 or more shield sections. But more then 4 would be extremely rare.
+Think, Firefly, millennium falcon, slave I (Boba fett's ship).
+
+They generally have 2 or more shield sections, but hardly ever more than 4.
+
 This class of ships is normally not fitted with jump or warp drives. But in some cases ships are modified to include these, or for certain roles it is build in.
 
 They are divided in 3 different sub-classes:
 * Cruiser: Weaponized frigates, focused on combat. These come in various roles.
-* Light transport: Small transports, like transporting up to 50 soldiers. Or a few diplomats. Depending on the role can have some weaponry.
+* Light transport: Small transports, like transporting up to 50 soldiers in spartan conditions or a few diplomats in luxury. Depending on the role can have some weaponry.
 * Support: Support types come in many variaties. They are simply a frigate hull fitted with whatever was needed. Anything from mine-layers to science vessels.
 ----------------------------------------------------------]]
 template = ShipTemplate():setName("Phobos T3"):setClass("Frigate", "Cruiser"):setModel("AtlasHeavyFighterYellow")
-template:setDescription([[Generic cruiser, not specialized in anything.]])
+template:setDescription([[The Phobos T3, just like the Atlantis, is the working horse of almost any navy. It's extremely easy to modify, which makes retro-fitting this ship a breeze. It's basic stats aren't all that impressive, but due to it's modular nature, it's fairly easy to produce in large quantities.]])
 template:setHull(70)
 template:setShields(50, 40)
 template:setSpeed(60, 10, 10)
@@ -23,14 +25,15 @@ template:setTubeDirection(0, -1)
 template:setTubeDirection(1,  1)
 
 variation = template:copy("Phobos M3"):setModel("AtlasHeavyFighterRed")
-variation:setDescription([[Upgraded Phobos T3 to include a mine subsystem]])
+variation:setDescription([[The Phonos M3 is one of the most comon variants on the Phonos T3. An extra mine tube was added to the ship. The extra storage space required for the mines does make this ship slightly slower.]])
 variation:setTubes(3, 10.0)
 variation:setWeaponStorage("Mine", 6)
+variation:setSpeed(55, 10, 10)
 variation:weaponTubeDisallowMissle(0, "Mine"):weaponTubeDisallowMissle(1, "Mine")
 variation:setTubeDirection(2,  180):setWeaponTubeExclusiveFor(2, "Mine")
 
 template = ShipTemplate():setName("Nirvana R5"):setClass("Frigate", "Cruiser: Anti Starfighter"):setModel("small_frigate_5") -- TODO: Better 3D model selection
-template:setDescription([[Anti fighter cruiser. Has a large amount of fast firing, low damage lasers to quickly take out starfighter class ships.]])
+template:setDescription([[The Nirvana R5 is an anti fighter cruiser. It has a large amount of fast firing, low damage point-defense weapons to quickly take out starfighters.]])
 template:setBeamWeapon(0, 90, -15, 1200, 3, 1)
 template:setBeamWeapon(1, 90,  15, 1200, 3, 1)
 template:setBeamWeapon(2, 90,  50, 1200, 3, 1)
@@ -48,7 +51,7 @@ variation:setBeamWeapon(3, 90, -50, 1200, 2.9, 1)
 variation:setSpeed(70, 15, 10)
 
 template = ShipTemplate():setName("Piranha F12"):setClass("Frigate", "Cruiser: Light Artillery"):setModel("HeavyCorvetteRed")
-template:setDescription([[Light artillery cruiser, smallest ship that is an exclusive broadside.]])
+template:setDescription([[Light artillery cruiser, smallest ship that is an exclusive broadside type ship.]])
 template:setHull(70)
 template:setShields(30, 30)
 template:setSpeed(40, 6, 8)
