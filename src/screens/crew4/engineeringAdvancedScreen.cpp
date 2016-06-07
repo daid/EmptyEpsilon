d822998b47574f7dd414dbd68cef90d3d7904ab2
oznogon <garrett+ozgh@oznogon.com>
2016-06-07 15:52:14 -0700

    Avoid shields button overlap on Eng+ on ships with all systems.

--- src/screens/crew4/engineeringAdvancedScreen.cpp
+++ src/screens/crew4/engineeringAdvancedScreen.cpp
@@ -5,5 +5,5 @@
 EngineeringAdvancedScreen::EngineeringAdvancedScreen(GuiContainer* owner)
 : EngineeringScreen(owner)
 {
-    (new GuiShieldsEnableButton(this, "SHIELDS_ENABLE"))->setPosition(20, 420, ATopLeft)->setSize(240, 50);
+    (new GuiShieldsEnableButton(this, "SHIELDS_ENABLE"))->setPosition(20, 370, ATopLeft)->setSize(240, 50);
 }
