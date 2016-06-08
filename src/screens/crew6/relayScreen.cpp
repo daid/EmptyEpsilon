659ff1f81098b3f00cac90737ef32186e0d2716e
oznogon <garrett+ozgh@oznogon.com>
2016-06-08 15:04:07 -0700

    Keep Relay's zoom slider in sync with the mouse wheel.

--- src/screens/crew6/relayScreen.cpp
+++ src/screens/crew6/relayScreen.cpp
@@ -80,7 +80,7 @@ RelayScreen::RelayScreen(GuiContainer* owner)
     info_faction = new GuiKeyValueDisplay(sidebar, "SCIENCE_FACTION", 0.4, "Faction", "");
     info_faction->setSize(GuiElement::GuiSizeMax, 30);
 
-    zoom_slider = new GuiSlider(this, "ZOOM_SLIDER", 50000.0f, 5000.0f, 50000.0f, [this](float value) {
+    zoom_slider = new GuiSlider(this, "ZOOM_SLIDER", 50000.0f, 6250.0f, 50000.0f, [this](float value) {
         zoom_label->setText("Zoom: " + string(50000.0f / value, 1.0f) + "x"); 
         radar->setDistance(value);
     });
@@ -157,9 +157,12 @@ void RelayScreen::onDraw(sf::RenderTarget& window)
         float view_distance = radar->getDistance() * (1.0 - (mouse_wheel_delta * 0.1f));
         if (view_distance > 50000.0f)
             view_distance = 50000.0f;
-        if (view_distance < 5000.0f)
-            view_distance = 5000.0f;
+        if (view_distance < 6250.0f)
+            view_distance = 6250.0f;
         radar->setDistance(view_distance);
+        // Keep the zoom slider in sync.
+        zoom_slider->setValue(view_distance);
+        zoom_label->setText("Zoom: " + string(50000.0f / view_distance, 1.0f) + "x");
     }
     ///!
 
