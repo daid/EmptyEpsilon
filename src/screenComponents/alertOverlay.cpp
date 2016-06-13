844adf627a5ea6f78cab401bea3ffb872fc9a3ae
Wrongtown <git@wrongtown.org>
2016-06-13 22:57:13 +0800

    Alert Overlay image amendment
    Suggest alteration to my last attempt at the Alert Overlay image; I've
    just removed top and bottom bars then ran it through pngcrush. As usual
    looking to make things cleaner based on feedback on bridgesim.net but
    welcome your opinion.

--- src/screenComponents/alertOverlay.cpp
+++ src/screenComponents/alertOverlay.cpp
@@ -20,12 +20,12 @@ void AlertLevelOverlay::onDraw(sf::RenderTarget& window)
     {
     case AL_RedAlert:
         color = sf::Color(255, 0, 0);
-        text = "RED ALERT";
+        text = "";
         text_size = 70;
         break;
     case AL_YellowAlert:
         color = sf::Color(255, 255, 0);
-        text = "YELLOW ALERT";
+        text = "";
         text_size = 60;
         break;
     case AL_Normal:
