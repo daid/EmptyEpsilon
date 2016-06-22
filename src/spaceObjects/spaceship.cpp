5376094e8a3eaf0caeefc0f900428dc7532ce02a
elptito <t.delclite@gmail.com>
2016-06-22 19:52:38 +0200

    Update spaceship.cpp
    SpaceShip::addBroadcast

--- src/spaceObjects/spaceship.cpp
+++ src/spaceObjects/spaceship.cpp
@@ -978,7 +978,7 @@ void SpaceShip::addBroadcast(int threshold, string message)
 
             if (addtolog)
             {
-                ship->addToShipLog(message, color);
+                ship->addToShipLog(message, color, "extern");
             }
         }
     }
