e91370230c9bd2df98bc2425fc7aa003e88f0286
daid <daid303@gmail.com>
2016-07-08 08:47:24 +0200

    Allow pseudo driver names to be specified as a list with semicoluns in between. So we can have a single hardware.ini configuration for windows and linux.

--- src/hardware/serialDriver.cpp
+++ src/hardware/serialDriver.cpp
@@ -537,11 +537,15 @@ string SerialPort::getPseudoDriverName(string port)
 
 std::vector<string> SerialPort::portsByPseudoDriverName(string driver_name)
 {
+    std::vector<string> driver_names = driver_name.split(";");
     std::vector<string> names;
-    for(string port : getAvailablePorts())
+    for(string driver : driver_names)
     {
-        if (getPseudoDriverName(port) == driver_name)
-            names.push_back(port);
+        for(string port : getAvailablePorts())
+        {
+            if (getPseudoDriverName(port) == driver)
+                names.push_back(port);
+        }
     }
     return names;
 }
