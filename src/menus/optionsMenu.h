fdbaba87b6fbdd0c724642a4f66775160ce62fd9
oznogon <garrett+ozgh@oznogon.com>
2016-07-12 15:52:19 -0700

    Revise options menu, add sound volume.

--- src/menus/optionsMenu.h
+++ src/menus/optionsMenu.h
@@ -1,12 +1,21 @@
-#ifndef OPTION_MENU_H
-#define OPTION_MENU_H
+#ifndef OPTIONS_MENU_H
+#define OPTIONS_MENU_H
 
 #include "gui/gui2_canvas.h"
 
+class GuiSlider;
+class GuiLabel;
+
 class OptionsMenu : public GuiCanvas
 {
+private:
+    GuiSlider* sound_volume_slider;
+    GuiSlider* music_volume_slider;
+    GuiLabel* sound_volume_overlay_label;
+    GuiLabel* music_volume_overlay_label;
 public:
     OptionsMenu();
-};
 
-#endif//MAIN_MENUS_H
+    void onKey(sf::Event::KeyEvent key, int unicode);
+};
+#endif//OPTIONS_MENU_H
