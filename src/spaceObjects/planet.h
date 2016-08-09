c4963ff24d07214166b633cc2424309038777460
daid <daid303@gmail.com>
2016-08-09 08:12:48 +0200

    Add the option to have planets orbit things (mainly other planets). Render planets always on the GM radar.

--- src/spaceObjects/planet.h
+++ src/spaceObjects/planet.h
@@ -15,6 +15,7 @@ public:
     virtual void draw3DTransparent() override;
 #endif//FEATURE_3D_RENDERING
     virtual void drawOnRadar(sf::RenderTarget& window, sf::Vector2f position, float scale, bool long_range) override;
+    virtual void drawOnGMRadar(sf::RenderTarget& window, sf::Vector2f draw_position, float scale, bool long_range) override;
     virtual void update(float delta) override;
     virtual void collide(Collisionable* target, float force) override;
     
@@ -25,7 +26,8 @@ public:
     void setPlanetRadius(float size);
     void setPlanetCloudRadius(float size);
     void setDistanceFromMovementPlane(float distance_from_movement_plane);
-    void setAxialRotationSpeed(float speed);
+    void setAxialRotationTime(float time);
+    void setOrbit(P<SpaceObject> target, float orbit_time);
     
     virtual string getExportLine() { return "Planet():setPosition(" + string(getPosition().x, 0) + ", " + string(getPosition().y, 0) + ")"; }
 
@@ -40,7 +42,10 @@ private:
     sf::Color atmosphere_color;
     float distance_from_movement_plane;
     
-    float axial_rotation_speed;
+    float axial_rotation_time;
+    int32_t orbit_target_id;
+    float orbit_time;
+    float orbit_distance;
     
     float collision_size;
 
