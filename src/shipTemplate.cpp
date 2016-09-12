02a6e1aa6d82039889c0d079a8434aef94d40ce9
daid <daid303@gmail.com>
2016-09-12 09:47:05 +0200

    Rename shield system to shield generators, to make a clear difference between shield generators and the actual shield levels. Fix the operations screen not being able to select things.

--- src/shipTemplate.cpp
+++ src/shipTemplate.cpp
@@ -301,8 +301,8 @@ string getSystemName(ESystem system)
     case SYS_Impulse: return "Impulse Engines";
     case SYS_Warp: return "Warp Drive";
     case SYS_JumpDrive: return "Jump Drive";
-    case SYS_FrontShield: return "Front Shields";
-    case SYS_RearShield: return "Rear Shields";
+    case SYS_FrontShield: return "Front Shield Generator";
+    case SYS_RearShield: return "Rear Shield Generator";
     default:
         return "UNKNOWN";
     }
