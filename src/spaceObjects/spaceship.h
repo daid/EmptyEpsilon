9de6a3ae5e5fd54bd56a5b46ffe81049c90d13ae
elptito <t.delclite@gmail.com>
2016-09-26 20:05:34 +0200

    Update spaceship.h

--- src/spaceObjects/spaceship.h
+++ src/spaceObjects/spaceship.h
@@ -14,7 +14,8 @@ enum EMainScreenSetting
     MSS_Right,
     MSS_Target,
     MSS_Tactical,
-    MSS_LongRange
+    MSS_LongRange,
+    MSS_ShipState
 };
 template<> void convert<EMainScreenSetting>::param(lua_State* L, int& idx, EMainScreenSetting& mss);
 
