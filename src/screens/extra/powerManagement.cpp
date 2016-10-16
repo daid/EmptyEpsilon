1087e383ed7f06f324b989352823323ec725d9a9
elptito <t.delclite@gmail.com>
2016-10-16 22:03:27 +0200

    qsd

--- src/screens/extra/powerManagement.cpp
+++ src/screens/extra/powerManagement.cpp
@@ -87,25 +87,34 @@ void PowerManagementScreen::onHotkey(const HotkeyResult& key)
 			for(int n=0; n<SYS_COUNT; n++)
 			{
 				if (key.hotkey == getSystemName(ESystem(n))+ string("_POWER_UP"))
-					if(my_spaceship->systems[n].power_request < 1.0f)
-						my_spaceship->commandSetSystemPowerRequest(ESystem(n), my_spaceship->systems[n].power_request + 0.5f);
+				{
+					if(my_spaceship->systems[n].power_request < 3.0f)
+					
+						my_spaceship->commandSetSystemPowerRequest(ESystem(n), my_spaceship->systems[n].power_request + 0.1f);
 					else
-						my_spaceship->commandSetSystemPowerRequest(ESystem(n), 1.0f);
+						my_spaceship->commandSetSystemPowerRequest(ESystem(n), 3.0f);
+				}
 				if (key.hotkey == getSystemName(ESystem(n))+ string("_POWER_DOWN"))
+				{
 					if(my_spaceship->systems[n].power_request > 0.0f)
-						my_spaceship->commandSetSystemPowerRequest(ESystem(n), my_spaceship->systems[n].power_request - 0.5f);
+						my_spaceship->commandSetSystemPowerRequest(ESystem(n), my_spaceship->systems[n].power_request - 0.1f);
 					else
 						my_spaceship->commandSetSystemPowerRequest(ESystem(n), 0.0f);
+				}
 				if (key.hotkey == getSystemName(ESystem(n))+ string("_COOLANT_UP"))
-					if(my_spaceship->systems[n].coolant_request < 1.0f)
+				{
+					if(my_spaceship->systems[n].coolant_request < 10.0f)
 						my_spaceship->commandSetSystemCoolantRequest(ESystem(n), my_spaceship->systems[n].coolant_request + 0.5f);
 					else
-						my_spaceship->commandSetSystemCoolantRequest(ESystem(n), 1.0f);	
+						my_spaceship->commandSetSystemCoolantRequest(ESystem(n), 10.0f);	
+				}
 				if (key.hotkey == getSystemName(ESystem(n))+ string("_COOLANT_DOWN"))
+				{
 					if(my_spaceship->systems[n].coolant_request > 0.0f)
 						my_spaceship->commandSetSystemCoolantRequest(ESystem(n), my_spaceship->systems[n].coolant_request - 0.5f);
 					else
 						my_spaceship->commandSetSystemCoolantRequest(ESystem(n), 0.0f);
+				}
 			}
 		}
 	}
