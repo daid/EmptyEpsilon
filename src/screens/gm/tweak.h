71b34e21a9c6421928690d36f2691f40d1947f5b
Thomas <t.delclite@gmail.com>
2016-11-08 14:02:18 +0100

    Update tweak.h

--- src/screens/gm/tweak.h
+++ src/screens/gm/tweak.h
@@ -162,6 +162,7 @@ private:
 
     GuiTextEntry* control_code;
     GuiSlider* reputation_point_slider;
+    GuiSlider* oxygen_point_slider;
     GuiSlider* energy_level_slider;
     GuiSlider* max_energy_level_slider;
     GuiLabel* position_count;
