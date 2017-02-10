a770e49dd479b0625c2e044340435feed993f6ce
greatestKeks <wolf.flo11235@gmail.com>
2017-02-10 19:54:58 +0100

    Adding default hotkey values
    the hotkeys are set are initialized as tuples containing name and a
    default hotkey value. Also I added 'F1' to see the hotkeys ingame
    as QUERTZ keyboards do not have a designated key for slash.

--- src/gui/hotkeyConfig.h
+++ src/gui/hotkeyConfig.h
@@ -8,10 +8,10 @@ class HotkeyConfigItem
 {
 public:
     string key;
-    string name;
+    std::tuple<string, string> value;
     sf::Event::KeyEvent hotkey;
     
-    HotkeyConfigItem(string key, string name);
+    HotkeyConfigItem(string key, std::tuple<string, string>);
     
     void load(string key_config);
 };
@@ -47,7 +47,7 @@ private:
     std::vector<HotkeyConfigCategory> categories;
     
     void newCategory(string key, string name);
-    void newKey(string key, string name);
+    void newKey(string key, std::tuple<string, string>);
 };
 
 extern HotkeyConfig hotkeys;
