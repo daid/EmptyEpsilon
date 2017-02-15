bddd46f3f01460c6a12c72d1869421e53ca6b059
Thomas <t.delclite@gmail.com>
2017-02-15 16:08:48 +0100

    Update shipSelectionScreen.cpp

--- src/menus/shipSelectionScreen.cpp
+++ src/menus/shipSelectionScreen.cpp
@@ -435,6 +435,7 @@ void ShipSelectionScreen::updateCrewTypeOptions()
         crew_position_button[powerManagement]->show();
         crew_position_button[databaseView]->show();
         crew_position_button[logView]->show();
+        crew_position_button[internLogView]->show();
         break;
     case 3:
         main_screen_button->hide();
