9b74d8fe1cce934057947bfea3b063e278038a1f
Amir Arad <amira@wix.com>
2018-09-21 11:14:23 +0300

    Intern (engineering) log feature from tdelc (#8)
    > I propose to add a intern log only seen by the engeneer, to display all intern modifications of the ship. I use it to inform the ship about a virus attack, or some ship failures.
    > 
    > * I change the function addToShipLog to addToShipLog(string message, sf::Color color, **string station**). If the value of the station is "extern", the extern log (for the relay) is filled. If "intern", the intern log (for the engeneer) is filled.
    > * By default, the value of the station is "extern". So this PR don't affect the current code
    > 
    > Some automatic informations fill the log with this color code :
    > 
    > Green :
    > shields enabled or disabled
    > Change of the shields frequency
    > 
    > Yellow :
    > Missile fire
    > Change of the beam system target
    > Change of the beam frequence
    > target activated
    > 
    > yellow alert
    > 
    > White :
    > warp speed
    > jump initialisation
    > 
    > Red :
    > damage received
    > red alert
    > auto destruction situation
    > 
    > Cyan :
    > docking situation
    > 
    > This PR don't use the new color system, because I don't understand it now :)
    > 
    > PS : I have this warning during compilation :
    > 
    > ```
    > /home/thomas/EE/EmptyEpsilon/src/spaceObjects/playerSpaceship.cpp:` In member function ‘const std::vector<PlayerSpaceship::ShipLogEntry>& PlayerSpaceship::getShipsLog(string) const’:
    > /home/thomas/EE/EmptyEpsilon/src/spaceObjects/playerSpaceship.cpp:667:1: warning: control reaches end of non-void function [-Wreturn-type]
    >  }
    > ```

--- src/spaceObjects/playerSpaceship.h
+++ src/spaceObjects/playerSpaceship.h
@@ -61,12 +61,13 @@ public:
         string prefix;
         string text;
         sf::Color color;
+        string station;
 
         ShipLogEntry() {}
-        ShipLogEntry(string prefix, string text, sf::Color color)
-        : prefix(prefix), text(text), color(color) {}
+        ShipLogEntry(string prefix, string text, sf::Color color, string station)
+        : prefix(prefix), text(text), color(color), station(station) {}
 
-        bool operator!=(const ShipLogEntry& e) { return prefix != e.prefix || text != e.text || color != e.color; }
+        bool operator!=(const ShipLogEntry& e) { return prefix != e.prefix || text != e.text || color != e.color || station != e.station; }
     };
     
     class CustomShipFunction
@@ -108,6 +109,7 @@ public:
     bool shields_active;
     // Password to join a ship. Default is empty.
     string control_code;
+    int warp_indicator;
 
 private:
     // soundManager index of the shield object
@@ -120,10 +122,11 @@ private:
     P<SpaceObject> comms_target; // Server only
     std::vector<int> comms_reply_id;
     std::vector<string> comms_reply_message;
-    CommsScriptInterface comms_script_interface; // Server only
-    // Ship's log container
-    std::vector<ShipLogEntry> ships_log;
-    
+    CommsScriptInterface comms_script_interface;  //Server only
+
+    std::vector<ShipLogEntry> ships_log_extern;
+    std::vector<ShipLogEntry> ships_log_intern;
+
 public:
     std::vector<CustomShipFunction> custom_functions;
 
@@ -263,10 +266,10 @@ public:
     float getNetSystemEnergyUsage();
 
     // Ship's log functions
-    void addToShipLog(string message, sf::Color color);
+    void addToShipLog(string message, sf::Color color, string station);
     void addToShipLogBy(string message, P<SpaceObject> target);
-    const std::vector<ShipLogEntry>& getShipsLog() const;
-
+    const std::vector<ShipLogEntry>& getShipsLog(string station) const;
+    
     // Ship's crew functions
     void transferPlayersToShip(P<PlayerSpaceship> other_ship);
     void transferPlayersAtPositionToShip(ECrewPosition position, P<PlayerSpaceship> other_ship);
