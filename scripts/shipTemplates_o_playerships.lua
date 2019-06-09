f9ad5da2ad47577371062b8651db283244962c77
tiittapauppi <tiittapauppi@gmail.com>
2019-06-09 15:38:03 +0300

    Add files via upload

--- scripts/shipTemplates_o_playerships.lua
+++ scripts/shipTemplates_o_playerships.lua
@@ -3,17 +3,18 @@ template = ShipTemplate():setName("Fighter F967"):setClass("Starfighter", "Inter
 template:setRadarTrace("radar_fighter.png")
 --             Arc, Dir, Range, CycleTime, Dmg
 template:setBeam(0, 40, -10, 1000.0, 6.0, 8)
-template:setBeam(1, 40,  10, 1000.0, 6.0, 8)
 template:setHull(60)
 template:setShields(500)
 template:setSpeed(40, 10, 20)
-template:setCombatManeuver(600, 0)
+template:setCombatManeuver(1200, 0)
 template:setWarpSpeed(0)
 template:setJumpDrive(false)
 template:setCloaking(false)
 template:setEnergyStorage(400)
-template:setTubes(2, 6.0) -- Amount of torpedo tubes, loading time
-template:setWeaponStorage("Homing", 200)
+template:setTubes(2, 4.0) -- Amount of torpedo tubes, loading time
+template:setTubeDirection(0, 0)
+template:setTubeDirection(1, 0)
+template:setWeaponStorage("Homing", 250)
 template:addRoomSystem(3, 0, 1, 1, "Maneuver");
 template:addDoor(0, 0, false);
 
@@ -34,9 +35,10 @@ template:setJumpDrive(false)
 template:setCloaking(false)
 template:setEnergyStorage(400)
 template:setWeaponStorage("Homing", 200)
-template:setWeaponStorage("EMP", 200)
+
 
 template:addRoomSystem(3, 0, 1, 1, "Maneuver");
+
 
 template:addDoor(0, 0, false);
 
@@ -45,21 +47,19 @@ template:addDoor(0, 0, false);
 template = ShipTemplate():setName("Corvette C743"):setClass("Corvette", "Destroyer"):setModel("eoc_odysseus"):setType("playership")
 template:setRadarTrace("radar_transport.png")
 --                  Arc, Dir, Range, CycleTime, Dmg
-template:setBeam(0, 90, -15, 1000.0, 6.0, 10)
-template:setBeam(1, 90,  15, 1000.0, 6.0, 10)
-template:setTubes(7, 8.0) -- Amount of torpedo tubes, and loading time of the tubes.
-template:setTubeDirection(0, -5)
-template:setTubeDirection(1, 5)
+template:setBeam(0, 0, -15, 1000.0, 6.0, 10)
+template:setTubes(6, 8.0) -- Amount of torpedo tubes, and loading time of the tubes.
+template:setTubeDirection(0, 0)
+template:setTubeDirection(1, 0)
 template:setTubeDirection(2, -45)
 template:setTubeDirection(3, 45)
-template:setTubeDirection(4, -90)
-template:setTubeDirection(6, 90)
-template:setTubeDirection(7, 180)
+template:setTubeDirection(4, -180)
+template:setTubeDirection(5, 180)
 
 
 template:setDockClasses("Starfighter", "Frigate")
 template:setHull(200)
-template:setShields(80, 80)
+template:setShields(500, 500)
 template:setSpeed(15, 4, 10)
 template:setWarpSpeed(0)
 template:setJumpDrive(false)
