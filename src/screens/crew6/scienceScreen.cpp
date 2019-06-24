26dd09fa2ebc14ad29247aad2a467b1a9cd43c17
tiittapauppi <tiittapauppi@gmail.com>
2019-06-24 11:38:07 +0300

    Radar to 100U

--- src/screens/crew6/scienceScreen.cpp
+++ src/screens/crew6/scienceScreen.cpp
@@ -41,7 +41,7 @@ ScienceScreen::ScienceScreen(GuiContainer* owner, ECrewPosition crew_position)
     radar_view->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
 
     // Draw the science radar.
-    science_radar = new GuiRadarView(radar_view, "SCIENCE_RADAR", 50000.0, &targets);
+    science_radar = new GuiRadarView(radar_view, "SCIENCE_RADAR", 100000.0, &targets);
     science_radar->setPosition(-270, 0, ACenterRight)->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
     science_radar->setRangeIndicatorStepSize(25000.0)->longRange()->enableWaypoints()->enableCallsigns()->enableHeadingIndicators()->setStyle(GuiRadarView::Circular)->setFogOfWarStyle(GuiRadarView::NebulaFogOfWar);
     science_radar->setCallbacks(
