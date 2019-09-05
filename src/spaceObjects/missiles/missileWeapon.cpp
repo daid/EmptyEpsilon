2b17c3360f094f3e3acbf147cbc34e8672b3c76a
Jaime van Kessel <nallath@gmail.com>
2019-09-05 17:41:02 +0200

    Ensure that small missiles get drawn smaller

--- src/spaceObjects/missiles/missileWeapon.cpp
+++ src/spaceObjects/missiles/missileWeapon.cpp
@@ -25,7 +25,7 @@ void MissileWeapon::drawOnRadar(sf::RenderTarget& window, sf::Vector2f position,
     object_sprite.setRotation(getRotation());
     object_sprite.setPosition(position);
     object_sprite.setColor(data.color);
-    object_sprite.setScale(0.5, 0.5);
+    object_sprite.setScale(category_modifier * 0.5, category_modifier * 0.5);
     window.draw(object_sprite);
 }
 
