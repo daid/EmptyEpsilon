362e6623fcdf53c418242493324a228ef1099a35
Juho Kuusisto <20241932+Tuhis@users.noreply.github.com>
2019-12-31 02:04:16 +0200

    Fix telegram finished notification

--- telegram_finished_notification.sh
+++ telegram_finished_notification.sh
@@ -28,7 +28,8 @@ send_msg "
 Travis build *${build_status}!*
 \`Repository:   ${TRAVIS_REPO_SLUG}\`
 \`Branch:       ${TRAVIS_BRANCH}\`
-\`EmptyEpsilon: https://tuhismedia.fi/ee/EmptyEpsilon.zip\`
+*EmptyEpsilon:*
+https://tuhismedia.fi/ee/EmptyEpsilon.zip
 *Commit Msg:*
 ${TRAVIS_COMMIT_MESSAGE}
 [Job Log here](${TRAVIS_JOB_WEB_URL})
