ba0c90498501f7f0887b580a79cb9c76c9325403
aBlueShadow <falter@mxdreamer>
2020-02-24 17:28:19 +0100

    adding shield frequency selector for engineering+
    also adding an autolayout to adjust to differrent widths

--- src/screens/crew4/engineeringAdvancedScreen.cpp
+++ src/screens/crew4/engineeringAdvancedScreen.cpp
@@ -1,9 +1,17 @@
 #include "engineeringAdvancedScreen.h"
 
+#include "gameGlobalInfo.h"
+#include "screenComponents/shieldFreqencySelect.h"
 #include "screenComponents/shieldsEnableButton.h"
 
 EngineeringAdvancedScreen::EngineeringAdvancedScreen(GuiContainer* owner)
 : EngineeringScreen(owner, engineeringAdvanced)
 {
-    (new GuiShieldsEnableButton(this, "SHIELDS_ENABLE"))->setPosition(20, 370, ATopLeft)->setSize(240, 50);
+	if (gameGlobalInfo->use_beam_shield_frequencies)
+    {
+        //The shield frequency selection includes a shield enable button.
+        (new GuiShieldFrequencySelect(this, "SHIELD_FREQ"))->setPosition(20, 310, ATopLeft)->setSize(240, 100);
+    }else{
+        (new GuiShieldsEnableButton(this, "SHIELDS_ENABLE"))->setPosition(20, 310, ABottomRight)->setSize(240, 50);
+    }
 }
