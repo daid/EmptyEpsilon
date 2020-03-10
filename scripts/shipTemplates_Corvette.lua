f9685236b2af3874a1fe7c056400ecdff6049cb4
Thomas L <lacombe.thomas@outlook.com>
2020-03-10 12:49:22 +0100

    localization

--- scripts/shipTemplates_Corvette.lua
+++ scripts/shipTemplates_Corvette.lua
@@ -11,7 +11,7 @@ They come in 3 different subclasses:
 
 --[[----------------------Destroyers----------------------]]
 
-template = ShipTemplate():setName("Atlantis X23"):setClass("Corvette", "Destroyer"):setModel("battleship_destroyer_1_upgraded")
+template = ShipTemplate():setName("Atlantis X23"):setLocaleName(_("Atlantis X23")):setClass(_("Corvette"), _("Destroyer")):setModel("battleship_destroyer_1_upgraded")
 template:setDescription(_([[The Atlantis X23 is the smallest model of destroyer, and its combination of frigate-like size and corvette-like power makes it an excellent escort ship when defending larger ships against multiple smaller enemies. Because the Atlantis X23 is fitted with a jump drive, it can also serve as an intersystem patrol craft.]]))
 template:setRadarTrace("radar_dread.png")
 template:setHull(100)
@@ -30,7 +30,7 @@ template:setTubeDirection(1, -90)
 template:setTubeDirection(2,  90)
 template:setTubeDirection(3,  90)
 
-variation = template:copy("Atlantis"):setType("playership")
+variation = template:copy("Atlantis"):setLocaleName(_("Atlantis")):setType("playership")
 variation:setDescription(_([[A refitted Atlantis X23 for more general tasks. The large shield system has been replaced with an advanced combat maneuvering systems and improved impulse engines. Its missile loadout is also more diverse. Mistaking the modified Atlantis for an Atlantis X23 would be a deadly mistake.]]))
 variation:setShields(200, 200)
 variation:setHull(250)
@@ -80,7 +80,7 @@ variation:addDoor(8, 4, false);
 --variation:addDoor(2, 2, false);
 --variation:addDoor(2, 5, false);
 
-template = ShipTemplate():setName("Starhammer II"):setClass("Corvette", "Destroyer"):setModel("battleship_destroyer_4_upgraded")
+template = ShipTemplate():setName("Starhammer II"):setLocaleName(_("Starhammer II")):setClass(_("Corvette"), _("Destroyer")):setModel("battleship_destroyer_4_upgraded")
 template:setDescription(_([[Contrary to its predecessor, the Starhammer II lives up to its name. By resolving the original Starhammer's power and heat management issues, the updated model makes for a phenomenal frontal assault ship. Its low speed makes it difficult to position, but when in the right place at the right time, even the strongest shields can't withstand a Starhammer's assault for long.]]))
 template:setRadarTrace("radar_dread.png")
 template:setHull(200)
@@ -98,7 +98,7 @@ template:setWeaponStorage("Homing", 4)
 template:setWeaponStorage("EMP", 2)
 template:weaponTubeDisallowMissle(1, "EMP")
 
-template = ShipTemplate():setName("Crucible"):setClass("Corvette","Popper"):setModel("LaserCorvetteRed"):setType("playership")
+template = ShipTemplate():setName("Crucible"):setLocaleName(_("Crucible")):setClass(_("Corvette"),_("Popper")):setModel("LaserCorvetteRed"):setType("playership")
 template:setDescription(_("A number of missile tubes range around this ship. Beams were deemed lower priority, though they are still present. Stronger defenses than a frigate, but not as strong as the Atlantis"))
 template:setRadarTrace("radar_laser.png")
 template:setHull(160)
@@ -154,7 +154,7 @@ template:addDoor(6, 3, false)
 template:addDoor(9, 2, true)
 template:addDoor(10,4, true)
 
-template = ShipTemplate():setName("Maverick"):setClass("Corvette","Gunner"):setModel("LaserCorvetteGreen"):setType("playership")
+template = ShipTemplate():setName("Maverick"):setLocaleName(_("Maverick")):setClass(_("Corvette"),_("Gunner")):setModel("LaserCorvetteGreen"):setType("playership")
 template:setDescription(_("A number of beams bristle from various points on this gunner. Missiles were deemed lower priority, though they are still present. Stronger defenses than a frigate, but not as strong as the Atlantis"))
 template:setRadarTrace("radar_laser.png")
 template:setHull(160)
@@ -214,7 +214,7 @@ template:addDoor(10,4, true)
 
 -- The weapons-platform is a stationary platform with beam-weapons. It's extremely slow to turn, but it's beam weapons do a huge amount of damage.
 -- Smaller ships can dock to this platform to re-supply.
-template = ShipTemplate():setName("Defense platform"):setClass("Corvette", "Support"):setModel("space_station_4")
+template = ShipTemplate():setName("Defense platform"):setLocaleName(_("Defense platform")):setClass(_("Corvette"), _("Support")):setModel("space_station_4")
 template:setDescription(_([[This stationary defense platform operates like a station, with docking and resupply functions, but is armed with powerful beam weapons and can slowly rotate. Larger systems often use these platforms to resupply patrol ships.]]))
 template:setRadarTrace("radartrace_smallstation.png")
 template:setHull(150)
@@ -232,7 +232,7 @@ template:setBeam(5, 30, 300, 4000.0, 1.5, 20)
 --[[----------------------Freighters----------------------]]
 
 for cnt=1,5 do
-    template = ShipTemplate():setName("Personnel Freighter " .. cnt):setClass("Corvette", "Freighter"):setModel("transport_1_" .. cnt)
+    template = ShipTemplate():setName("Personnel Freighter " .. cnt):setLocaleName(_("Personnel Freighter ") .. cnt):setClass(_("Corvette"), _(_("Freighter"))):setModel("transport_1_" .. cnt)
     template:setDescription(_([[These freighters are designed to transport armed troops, military support personnel, and combat gear.]]))
     template:setHull(100)
     template:setShields(50, 50)
@@ -240,11 +240,11 @@ for cnt=1,5 do
     template:setRadarTrace("radar_transport.png")
     
     if cnt > 2 then
-        variation = template:copy("Personnel Jump Freighter " .. cnt)
+        variation = template:copy("Personnel Jump Freighter " .. cnt):setLocaleName(_("Personnel Jump Freighter ") .. cnt)
         variation:setJumpDrive(true)
     end
 
-    template = ShipTemplate():setName("Goods Freighter " .. cnt):setClass("Corvette", "Freighter"):setModel("transport_2_" .. cnt)
+    template = ShipTemplate():setName("Goods Freighter " .. cnt):setLocaleName(_("Goods Freighter ") .. cnt):setClass(_("Corvette"), _("Freighter")):setModel("transport_2_" .. cnt)
     template:setDescription(_([[Cargo freighters haul large loads of cargo across long distances on impulse power. Their cargo bays include climate control and stabilization systems that keep the cargo in good condition.]]))
     template:setHull(100)
     template:setShields(50, 50)
@@ -252,11 +252,11 @@ for cnt=1,5 do
     template:setRadarTrace("radar_transport.png")
     
     if cnt > 2 then
-        variation = template:copy("Goods Jump Freighter " .. cnt)
+        variation = template:copy("Goods Jump Freighter " .. cnt):setLocaleName(_("Goods Jump Freighter ") .. cnt)
         variation:setJumpDrive(true)
     end
     
-    template = ShipTemplate():setName("Garbage Freighter " .. cnt):setClass("Corvette", "Freighter"):setModel("transport_3_" .. cnt)
+    template = ShipTemplate():setName("Garbage Freighter " .. cnt):setLocaleName(_("Garbage Freighter ") .. cnt):setClass(_("Corvette"), _("Freighter")):setModel("transport_3_" .. cnt)
     template:setDescription(_([[These freighters are specially designed to haul garbage and waste. They are fitted with a trash compactor and fewer stabilzation systems than cargo freighters.]]))
     template:setHull(100)
     template:setShields(50, 50)
@@ -264,11 +264,11 @@ for cnt=1,5 do
     template:setRadarTrace("radar_transport.png")
     
     if cnt > 2 then
-        variation = template:copy("Garbage Jump Freighter " .. cnt)
+        variation = template:copy("Garbage Jump Freighter " .. cnt):setLocaleName(_("Garbage Jump Freighter ") .. cnt)
         variation:setJumpDrive(true)
     end
 
-    template = ShipTemplate():setName("Equipment Freighter " .. cnt):setClass("Corvette", "Freighter"):setModel("transport_4_" .. cnt)
+    template = ShipTemplate():setName("Equipment Freighter " .. cnt):setLocaleName(_("Equipment Freighter ") .. cnt):setClass(_("Corvette"), _("Freighter")):setModel("transport_4_" .. cnt)
     template:setDescription(_([[Equipment freighters have specialized environmental and stabilization systems to safely carry delicate machinery and complex instruments.]]))
     template:setHull(100)
     template:setShields(50, 50)
@@ -276,11 +276,11 @@ for cnt=1,5 do
     template:setRadarTrace("radar_transport.png")
     
     if cnt > 2 then
-        variation = template:copy("Equipment Jump Freighter " .. cnt)
+        variation = template:copy("Equipment Jump Freighter " .. cnt):setLocaleName(_("Equipment Jump Freighter ") .. cnt)
         variation:setJumpDrive(true)
     end
 
-    template = ShipTemplate():setName("Fuel Freighter " .. cnt):setClass("Corvette", "Freighter"):setModel("transport_5_" .. cnt)
+    template = ShipTemplate():setName("Fuel Freighter " .. cnt):setLocaleName(_("Fuel Freighter ") .. cnt):setClass(_("Corvette"), _("Freighter")):setModel("transport_5_" .. cnt)
     template:setDescription(_([[Fuel freighters have massive tanks for hauling fuel, and delicate internal sensors that watch for any changes to their cargo's potentially volatile state.]]))
     template:setHull(100)
     template:setShields(50, 50)
@@ -288,12 +288,12 @@ for cnt=1,5 do
     template:setRadarTrace("radar_transport.png")
     
     if cnt > 2 then
-        variation = template:copy("Fuel Jump Freighter " .. cnt)
+        variation = template:copy("Fuel Jump Freighter " .. cnt):setLocaleName(_("Fuel Jump Freighter ") .. cnt)
         variation:setJumpDrive(true)
     end
 end
 
-template = ShipTemplate():setName("Jump Carrier"):setClass("Corvette", "Freighter"):setModel("transport_4_2")
+template = ShipTemplate():setName("Jump Carrier"):setLocaleName(_("Jump Carrier")):setClass(_("Corvette"), _("Freighter")):setModel("transport_4_2")
 template:setDescription(_([[The Jump Carrier is a specialized Freighter. It does not carry any cargo, as it's cargo bay is taken up by a specialized jump drive and the energy storage required to run this jump drive.
 It is designed to carry other ships deep into space. So it has special docking parameters, allowing other ships to attach themselves to this ship.]]))
 template:setHull(100)
@@ -305,7 +305,7 @@ template:setJumpDriveRange(5000, 100 * 50000) -- The jump carrier can jump a 100
 template:setDockClasses("Starfighter", "Frigate", "Corvette")
 
 
-variation = template:copy("Benedict"):setType("playership"):setClass("Corvette","Freighter/Carrier")
+variation = template:copy("Benedict"):setLocaleName(_("Benedict")):setType("playership"):setClass(_("Corvette"), _("Freighter/Carrier"))
 variation:setDescription(_("Benedict is an improved version of the Jump Carrier"))
 variation:setShields(70, 70)
 variation:setHull(200)
@@ -339,7 +339,7 @@ variation:addDoor(2, 3, false)
 variation:addDoor(8, 2, false)
 variation:addDoor(9, 3, false)
 
-var2 = variation:copy("Kiriya")
+var2 = variation:copy("Kiriya"):setLocaleName(_("Kiriya"))
 var2:setDescription(_("Kiriya is an improved warp drive version of the Jump Carrier"))
 --          Arc, Dir, Range, CycleTime, Dmg
 var2:setBeam(0, 10,   0, 1500.0, 6.0, 4)
