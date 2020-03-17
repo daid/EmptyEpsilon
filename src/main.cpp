2d254b8dad5ac2196afd292c287324b905dceb63
Daid <daid303@gmail.com>
2020-03-17 22:25:51 +0100

    Properly build with libopus. And create a network audio recorder so we can test voice communication.

--- src/main.cpp
+++ src/main.cpp
@@ -116,6 +116,7 @@ int main(int argc, char** argv)
     }
 
     new Engine();
+    (new NetworkAudioRecorder())->setKeyActivation(sf::Keyboard::Key::Tab);
 
     if (PreferencesManager::get("proxy") != "")
     {
