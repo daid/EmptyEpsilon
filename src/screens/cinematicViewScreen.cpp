4dbb0af315691aa4b4fac78610e44cae9dde350e
Daid <daid303@gmail.com>
2020-03-18 22:19:17 +0100

    Small tweaks to the cinematic view.

--- src/screens/cinematicViewScreen.cpp
+++ src/screens/cinematicViewScreen.cpp
@@ -145,11 +145,13 @@ void CinematicViewScreen::update(float delta)
         // float target_velocity = sf::length(target->getVelocity());
 
         // We want the camera to always be less than 1U from the selected ship.
-        max_camera_distance = 1000.0f + target->getRadius();
+        max_camera_distance = 1000.0f + target->getRadius() + sf::length(target->getVelocity());
         min_camera_distance = target->getRadius() * 2.0f;
 
         // Check if our selected ship has a weapons target.
         target_of_target = target->getTarget();
+        if (target_of_target && sf::length(target_of_target->getPosition() - target_position_2D) > 10000.0)
+            target_of_target = nullptr;
 
         // If it does, lock the camera onto that target.
         if (camera_lock_tot_toggle->getValue() && target_of_target)
