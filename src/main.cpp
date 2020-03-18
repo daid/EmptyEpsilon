f26c2b035e08a613615f342c3727e8e5a1525063
Daid <daid303@gmail.com>
2020-03-18 15:59:40 +0100

    Use the new VC key api and switch the tab button for tilde, as tab already has a use.

--- src/main.cpp
+++ src/main.cpp
@@ -116,7 +116,6 @@ int main(int argc, char** argv)
     }
 
     new Engine();
-    (new NetworkAudioRecorder())->setKeyActivation(sf::Keyboard::Key::Tab);
 
     if (PreferencesManager::get("proxy") != "")
     {
@@ -282,6 +281,8 @@ int main(int argc, char** argv)
             }
         }
     }
+    NetworkAudioRecorder* nar = new NetworkAudioRecorder();
+    nar->addKeyActivation(sf::Keyboard::Key::Tilde, 0);
 
     P<HardwareController> hardware_controller = new HardwareController();
 #ifdef CONFIG_DIR
