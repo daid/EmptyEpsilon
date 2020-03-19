166018ecffab8b9e1fb9a52ea51055c3ca1b93d3
aBlueShadow <falter@mxdreamer>
2020-03-19 19:05:08 +0100

    add i18n for frequency-less shields

--- src/screenComponents/shieldsEnableButton.cpp
+++ src/screenComponents/shieldsEnableButton.cpp
@@ -40,13 +40,11 @@ void GuiShieldsEnableButton::onDraw(sf::RenderTarget& window)
             button->show();
             button->setValue(my_spaceship->shields_active);
             bar->hide();
-            if (gameGlobalInfo->use_beam_shield_frequencies)
-	    {
-		string shield_status=my_spaceship->shields_active ? tr("shields","ON") : tr("shields","OFF");
-		button->setText(tr("{frequency} Shields: {status}").format({{"frequency", frequencyToString(my_spaceship->shield_frequency)}, {"status", shield_status}}));
-            }
-            else
-                button->setText(my_spaceship->shields_active ? " Shields: ON" : " Shields: OFF");
+			string shield_status=my_spaceship->shields_active ? tr("shields","ON") : tr("shields","OFF");
+			if (gameGlobalInfo->use_beam_shield_frequencies)
+				button->setText(tr("{frequency} Shields: {status}").format({{"frequency", frequencyToString(my_spaceship->shield_frequency)}, {"status", shield_status}}));
+			else
+				button->setText(tr("Shields: {status}").format({{"status", shield_status}}));
         }
     }
 }
