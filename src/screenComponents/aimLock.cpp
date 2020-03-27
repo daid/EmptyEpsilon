5080b382d9d33da13d7bd9992f926942f089b66c
Jonathan <JonathanILevi@gmail.com>
2020-03-27 11:20:57 -0500

    Bit of a hacky solution for weapons manual targeting rotating with view.

--- src/screenComponents/aimLock.cpp
+++ src/screenComponents/aimLock.cpp
@@ -52,3 +52,70 @@ void AimLockButton::setAimLock(bool value)
         this->tube_controls->setMissileTargetAngle(my_spaceship->getRotation());
     }
 }
+
+
+AimLock::AimLock(GuiContainer* owner, string id, GuiRadarView* radar, float min_value, float max_value, float start_value, func_t func)
+: GuiRotationDial(owner, id, min_value, max_value, start_value, func), radar(radar)
+{
+}
+
+void AimLock::onDraw(sf::RenderTarget& window)
+{
+    sf::Vector2f center = getCenterPoint();
+    float view_rotation = radar->getViewRotation();
+    float radius = std::min(rect.width, rect.height) / 2.0f;
+    
+    sf::Sprite sprite;
+    textureManager.setTexture(sprite, "dial_background.png");
+    sprite.setPosition(center);
+    sprite.setScale(radius * 2 / sprite.getTextureRect().height, radius * 2 / sprite.getTextureRect().height);
+    window.draw(sprite);
+
+    textureManager.setTexture(sprite, "dial_button.png");
+    sprite.setPosition(center);
+    sprite.setScale(radius * 2 / sprite.getTextureRect().height, radius * 2 / sprite.getTextureRect().height);
+    sprite.setRotation((value - min_value) / (max_value - min_value) * 360.0f - view_rotation);
+    window.draw(sprite);
+}
+
+bool AimLock::onMouseDown(sf::Vector2f position)
+{
+    return GuiRotationDial::onMouseDown(position);
+}
+
+void AimLock::onMouseDrag(sf::Vector2f position)
+{
+    float view_rotation = radar->getViewRotation();
+
+    sf::Vector2f center = getCenterPoint();
+    
+    sf::Vector2f diff = position - center;
+
+    float new_value = ((sf::vector2ToAngle(diff) + 90.0f) + view_rotation) / 360.0f;
+    if (new_value < 0.0f)
+        new_value += 1.0f;
+    new_value = min_value + (max_value - min_value) * new_value;
+    if (min_value < max_value)
+    {
+        if (new_value < min_value)
+            new_value = min_value;
+        if (new_value > max_value)
+            new_value = max_value;
+    }else{
+        if (new_value > min_value)
+            new_value = min_value;
+        if (new_value < max_value)
+            new_value = max_value;
+    }
+    if (value != new_value)
+    {
+        value = new_value;
+        if (func)
+            func(value);
+    }
+}
+
+void AimLock::onMouseUp(sf::Vector2f position)
+{
+}
+
--- src/screenComponents/aimLock.h
+++ src/screenComponents/aimLock.h
@@ -2,9 +2,10 @@
 #define AIM_LOCK_H
 
 #include "gui/gui2_togglebutton.h"
+#include "gui/gui2_rotationdial.h"
+#include "screenComponents/radarView.h"
 
 class GuiMissileTubeControls;
-class GuiRotationDial;
 
 class AimLockButton : public GuiToggleButton
 {
@@ -17,6 +18,19 @@ private:
     GuiRotationDial* missile_aim;
     
     void setAimLock(bool value);
+};
+
+
+class AimLock : public GuiRotationDial {
+public:
+    AimLock(GuiContainer* owner, string id, GuiRadarView* radar, float min_value, float max_value, float start_value, func_t func);
+
+    virtual void onDraw(sf::RenderTarget& window);
+    virtual bool onMouseDown(sf::Vector2f position);
+    virtual void onMouseDrag(sf::Vector2f position);
+    virtual void onMouseUp(sf::Vector2f position);
+private:
+    GuiRadarView* radar;
 };
 
 #endif//AIM_LOCK_H
