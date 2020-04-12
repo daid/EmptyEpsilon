a6f8a4c280b2750ed304a76e9783a2dd4402d772
Oznogon <garrett+ozgh@oznogon.com>
2020-04-12 15:34:11 -0700

    Edit Hornets, Adders

--- scripts/shipTemplates_Starfighters.lua
+++ scripts/shipTemplates_Starfighters.lua
@@ -36,80 +36,145 @@ template = ShipTemplate():setName("MT52 Hornet")
 
     -- Maneuverability
     --   Impulse Forward, Turn, Acceleration
-    template:setSpeed(120, 30, 25)
---                  Arc, Dir, Range, CycleTime, Dmg
-template:setBeam(0, 30, 0, 700.0, 4.0, 2)
+    template:setSpeed(120,  30, 25)
 
-variation = template:copy("MU52 Hornet"):setLocaleName(_("MU52 Hornet"))
-variation:setModel("WespeScoutRed")
-variation:setDescription(_([[The MU52 Hornet is a new, upgraded version of the MT52. All of its systems are slightly improved over the MT52 model.]]))
-variation:setHull(35)
-variation:setShields(22)
-variation:setSpeed(125, 32, 25)
-variation:setBeam(0, 30, 0, 900.0, 4.0, 2.5)
+    -- Weapons
+    --   Beams            ID, Arc, Bear,  Range, Cycle, Damage
+    template:setBeamWeapon(0,  30,    0,  700.0,   4.0, 2)
 
-variation = variation:copy("MP52 Hornet"):setLocaleName(_("MP52 Hornet")):setType("playership")
-variation:setDescription(_([[The MP52 Hornet is a significantly upgraded version of MU52 Hornet, with nearly twice the hull strength, nearly three times the shielding, better acceleration, impulse boosters, and a second laser cannon.]]))
-variation:setImpulseSoundFile("sfx/engine_fighter.wav")
-variation:setHull(70)
-variation:setShields(60)
-variation:setSpeed(125, 32, 40)
-variation:setCombatManeuver(600, 0)
-variation:setBeam(0, 30, 5, 900.0, 4.0, 2.5)
-variation:setBeam(1, 30,-5, 900.0, 4.0, 2.5)
-variation:setEnergyStorage(400)
+------------------------MU52 Hornet-------------------------
+variation = template:copy("MU52 Hornet")
+    variation:setLocaleName(_("MU52 Hornet"))
+    variation:setModel("WespeScoutRed")
+    variation:setDescription(_([[The MU52 Hornet is a new, upgraded version of the MT52. All of its systems are slightly improved over the MT52 model.]]))
 
-variation:setRepairCrewCount(1)
-variation:addRoomSystem(3, 0, 1, 1, "Maneuver");
-variation:addRoomSystem(1, 0, 2, 1, "BeamWeapons");
+    -- Defenses
+    variation:setHull(35)
+    variation:setShields(22)
 
-variation:addRoomSystem(0, 1, 1, 2, "RearShield");
-variation:addRoomSystem(1, 1, 2, 2, "Reactor");
-variation:addRoomSystem(3, 1, 2, 1, "Warp");
-variation:addRoomSystem(3, 2, 2, 1, "JumpDrive");
-variation:addRoomSystem(5, 1, 1, 2, "FrontShield");
+    -- Maneuverability
+    --   Impulse   Forward, Turn, Acceleration
+    variation:setSpeed(125,   32, 25)
 
-variation:addRoomSystem(1, 3, 2, 1, "MissileSystem");
-variation:addRoomSystem(3, 3, 1, 1, "Impulse");
+    -- Weapons
+    --   Beams            ID, Arc, Bear,  Range, Cycle, Damage
+    template:setBeamWeapon(0,  30,    0,  900.0,   4.0, 2.5)
 
-variation:addDoor(2, 1, true);
-variation:addDoor(3, 1, true);
-variation:addDoor(1, 1, false);
-variation:addDoor(3, 1, false);
-variation:addDoor(3, 2, false);
-variation:addDoor(3, 3, true);
-variation:addDoor(2, 3, true);
-variation:addDoor(5, 1, false);
-variation:addDoor(5, 2, false);
+-----------------MP52 Hornet (player ship)------------------
+variation = variation:copy("MP52 Hornet")
+    variation:setLocaleName(_("MP52 Hornet"))
+    variation:setDescription(_([[The MP52 Hornet is a significantly upgraded version of MU52 Hornet, with nearly twice the hull strength, nearly three times the shielding, better acceleration, impulse boosters, and a second laser cannon.]]))
+    variation:setImpulseSoundFile("sfx/engine_fighter.wav")
+    variation:setType("playership")
 
-template = ShipTemplate():setName("Adder MK5"):setLocaleName(_("Adder MK5")):setClass(_("Starfighter"), _("Gunship")):setModel("AdlerLongRangeScoutYellow")
-template:setRadarTrace("radar_fighter.png")
-template:setDescription(_([[The Adder line's fifth iteration proved to be a great success among pirates and law officers alike. It is cheap, fast, and easy to maintain, and it packs a decent punch.]]))
-template:setHull(50)
-template:setShields(30)
-template:setSpeed(80, 28, 25)
-template:setBeam(0, 35, 0, 800, 5.0, 2.0)
-template:setBeam(1, 70, 30, 600, 5.0, 2.0)
-template:setBeam(2, 70, -35, 600, 5.0, 2.0)
-template:setTubes(1, 15.0)
-template:setTubeSize(0, "small")
-template:setWeaponStorage("HVLI", 4)
+    -- Capabilities
+    variation:setEnergyStorage(400)
 
-variation = template:copy("Adder MK4"):setLocaleName(_("Adder MK4"))
-variation:setModel("AdlerLongRangeScoutBlue")
-variation:setDescription(_([[The mark 4 Adder is a rare sight these days due to the success its successor, the mark 5 Adder, which often replaces this model. Its similar hull, however, means careless buyers are sometimes conned into buying mark 4 models disguised as the mark 5.]]))
-variation:setHull(40)
-variation:setShields(20)
-variation:setSpeed(60, 20, 20)
-variation:setTubes(1, 20.0)
-variation:setTubeSize(0, "small")
-variation:setWeaponStorage("HVLI", 2)
+    -- Defenses
+    variation:setHull(70)
+    variation:setShields(60)
 
-variation = template:copy("Adder MK6"):setLocaleName(_("Adder MK6"))
-variation:setModel("AdlerLongRangeScoutRed")
-variation:setDescription(_([[The mark 6 Adder is a small upgrade compared to the highly successful mark 5 model. Since people still prefer the more familiar and reliable mark 5, the mark 6 has not seen the same level of success.]]))
-variation:setBeam(3, 35,180, 600, 6.0, 2.0)
-variation:setWeaponStorage("HVLI", 8)
+    -- Maneuverability
+    --   Impulse   Forward, Turn, Acceleration
+    variation:setSpeed(125,   32, 40)
+    --   Combat Maneuver      Boost, Strafe
+    variation:setCombatManeuver(600, 0)
+    -- Weapons
+    --   Beams             ID, Arc, Bear,  Range, Cycle, Damage
+    variation:setBeamWeapon(0,  30,    5,  900.0,   4.0, 2.5)
+    variation:setBeamWeapon(1,  30,   -5,  900.0,   4.0, 2.5)
+
+    -- Internal layout
+    --   Repair crew count
+    variation:setRepairCrewCount(1)
+    --   Rooms          Position  Size
+    --                      X  Y  W  H  System
+    variation:addRoomSystem(3, 0, 1, 1, "Maneuver");
+    variation:addRoomSystem(1, 0, 2, 1, "BeamWeapons");
+
+    variation:addRoomSystem(0, 1, 1, 2, "RearShield");
+    variation:addRoomSystem(1, 1, 2, 2, "Reactor");
+    variation:addRoomSystem(3, 1, 2, 1, "Warp");
+    variation:addRoomSystem(3, 2, 2, 1, "JumpDrive");
+    variation:addRoomSystem(5, 1, 1, 2, "FrontShield");
+
+    variation:addRoomSystem(1, 3, 2, 1, "MissileSystem");
+    variation:addRoomSystem(3, 3, 1, 1, "Impulse");
+
+    --   Doors    Position
+    --                X  Y  Horizontal?
+    variation:addDoor(2, 1, true);
+    variation:addDoor(3, 1, true);
+    variation:addDoor(1, 1, false);
+    variation:addDoor(3, 1, false);
+    variation:addDoor(3, 2, false);
+    variation:addDoor(3, 3, true);
+    variation:addDoor(2, 3, true);
+    variation:addDoor(5, 1, false);
+    variation:addDoor(5, 2, false);
+
+--------------------------GUNSHIPS--------------------------
+
+-------------------------Adder MK5--------------------------
+template = ShipTemplate():setName("Adder MK5")
+    template:setLocaleName(_("Adder MK5"))
+    template:setClass(_("Starfighter"), _("Gunship"))
+    template:setModel("AdlerLongRangeScoutYellow")
+    template:setDescription(_([[The Adder line's fifth iteration proved to be a great success among pirates and law officers alike. It is cheap, fast, and easy to maintain, and it packs a decent punch.]]))
+    template:setRadarTrace("radar_fighter.png")
+
+    -- Defenses
+    template:setHull(50)
+    template:setShields(30)
+
+    -- Maneuverability
+    --   Impulse Forward, Turn, Acceleration
+     template:setSpeed(80,  28, 25)
+    -- Weapons
+    --   Beams            ID, Arc, Bear,  Range, Cycle, Damage
+    template:setBeamWeapon(0,  35,    0,  800.0,   5.0, 2.0)
+    template:setBeamWeapon(1,  70,   30,  600.0,   5.0, 2.0)
+    template:setBeamWeapon(2,  70,  -35,  600.0,   5.0, 2.0)
+    --   Tubes    Count, Load Time
+    template:setTubes(1, 15.0)
+    --     Tube size    ID, Size (small, medium, large)
+    template:setTubeSize(0, "small")
+    --     Tube weapon storage    Type, Count
+    template:setWeaponStorage(  "HVLI", 4)
+
+-------------------------Adder MK4--------------------------
+variation = template:copy("Adder MK4")
+    variation:setLocaleName(_("Adder MK4"))
+    variation:setModel("AdlerLongRangeScoutBlue")
+    variation:setDescription(_([[The mark 4 Adder is a rare sight these days due to the success its successor, the mark 5 Adder, which often replaces this model. Its similar hull, however, means careless buyers are sometimes conned into buying mark 4 models disguised as the mark 5.]]))
+
+    -- Defenses
+    variation:setHull(40)
+    variation:setShields(20)
+
+    -- Maneuverability
+    --   Impulse  Forward, Turn, Acceleration
+    variation:setSpeed(60, 20, 20)
+
+    -- Weapons
+    --   Tubes     Count, Load Time
+    variation:setTubes(1, 20.0)
+    --     Tube size     ID, Size (small, medium, large)
+    variation:setTubeSize(0, "small")
+    --     Tube weapon storage     Type, Count
+    variation:setWeaponStorage(  "HVLI", 2)
+
+-------------------------Adder MK6--------------------------
+variation = template:copy("Adder MK6")
+    variation:setLocaleName(_("Adder MK6"))
+    variation:setModel("AdlerLongRangeScoutRed")
+    variation:setDescription(_([[The mark 6 Adder is a small upgrade compared to the highly successful mark 5 model. Since people still prefer the more familiar and reliable mark 5, the mark 6 has not seen the same level of success.]]))
+
+    -- Weapons
+    --   Beams             ID, Arc, Bear,  Range, Cycle, Damage
+    variation:setBeamWeapon(3,  35,  180,  600.0,   6.0, 2.0)
+    --     Tube weapon storage     Type, Count
+    variation:setWeaponStorage(  "HVLI", 8)
 
 template = ShipTemplate():setName("WX-Lindworm"):setLocaleName(_("WX-Lindworm")):setClass(_("Starfighter"), _("Bomber")):setModel("LindwurmFighterYellow")
 template:setRadarTrace("radar_fighter.png")
