03566160e9d13b7e0100ab1763d9f14bc52aaab9
aBlueShadow <falter@mxdreamer>
2020-04-12 10:35:54 +0200

    translation markers for old and dreadnought templates

--- scripts/shipTemplates_Dreadnaught.lua
+++ scripts/shipTemplates_Dreadnaught.lua
@@ -1,22 +1,22 @@
---[[                  Dreadnaught
-Dreadnaughts are the largest ships.
+--[[                  Dreadnought
+Dreadnoughts are the largest ships.
 They are so large and uncommon that every type is pretty much their own subclass.
 They usually come with 6 or more shield sections, require a crew of 250+ to operate.
 
 Think: Stardestroyer.
 ----------------------------------------------------------]]
 
-template = ShipTemplate():setName("Odin"):setClass("Dreadnaught", "Odin"):setModel("space_station_2")
+template = ShipTemplate():setName("Odin"):setLocaleName(_("Odin")):setClass(_("class", "Dreadnought"), _("subclass","Odin")):setModel("space_station_2")
 template:setRadarTrace("radartrace_largestation.png")
-template:setDescription([[The Odin is a "ship" so large and unique that it's almost a class of its own.
+template:setDescription(_([[The Odin is a "ship" so large and unique that it's almost a class of its own.
 
 The ship is often nicknamed the "all-father", a name that aptly describes the many roles this ship can fulfill. It's both a supply station and an extremely heavily armored and shielded weapon station capable of annihilating small fleets on its own.
 
 Odin's core contains the largest jump drive ever created. About 150 support crew are needed to operate the jump drive alone, and it takes 5 days of continuous operation to power it.
 
-Due to the enormous cost of this dreadnaught, only the richest star systems are able to build and maintain ships like the Odin. 
+Due to the enormous cost of this Dreadnought, only the richest star systems are able to build and maintain ships like the Odin.
 
-This machine's primary tactic is to jump into an unsuspecting enemy system and destroy everything before they know what hit them. It's effective and destructive, but extremely expensive.]])
+This machine's primary tactic is to jump into an unsuspecting enemy system and destroy everything before they know what hit them. It's effective and destructive, but extremely expensive.]]))
 template:setJumpDrive(true)
 template:setTubes(16, 3.0)
 template:setWeaponStorage("Homing", 1000)
