f70cb8f70462685a2eb66c5f85d8cc7002deb7d0
Oznogon <garrett+ozgh@oznogon.com>
2020-04-12 12:55:17 -0700

    Edit shipTemplates.lua

--- scripts/shipTemplates.lua
+++ scripts/shipTemplates.lua
@@ -1,20 +1,21 @@
---[[
-Base of the ship templates, from here different classes of ships are included to be used within the game.
-Each sub-file defines it's own set of ship classes.
+--[[ Base Ship Templates
+This file includes ship templates for each ship class to be used within the game.
+Each sub-file also defines its own set of ship subclasses.
 
 These are:
+
 * Stations: For different kinds of space stations, from tiny to huge.
 * Starfighters: Smallest ships in the game.
-* Frigates: Medium sized ships. Operate on a small crew.
-* Covette: Large, slower, less maneuverable ships.
-* Dreadnaught: Huge things. Everything in here is really really big, and generally really really deadly.
+* Frigates: Medium-sized ships that operate with a small crew.
+* Covettes: Large, slower, less maneuverable ships.
+* Dreadnoughts: Huge things. Everything in here is really really big, and generally really really deadly.
 
-Player ships are in general large frigates to small corvette class
+Most player ships range from large frigates to small corvettes.
 --]]
 require("shipTemplates_Stations.lua")
 require("shipTemplates_Starfighters.lua")
 require("shipTemplates_Frigates.lua")
 require("shipTemplates_Corvettes.lua")
 require("shipTemplates_Dreadnoughts.lua")
---For now, we add our old ship templates as well. These should be removed at some point.
+--[[ For now, we also add our old ship templates. These should be removed at some point. ]]
 require("shipTemplates_OLD.lua")
