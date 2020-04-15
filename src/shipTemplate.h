0b8d9b20902e2e37c27968d7e6a4aeb0b4ccc091
aBlueShadow <jilg@arcor.de>
2020-04-15 20:14:49 +0200

    remove reduncy

--- src/shipTemplate.h
+++ src/shipTemplate.h
@@ -69,14 +69,14 @@ public:
 private:
     static std::unordered_map<string, P<ShipTemplate> > templateMap;
     string name;
-    string locale_name;
+    string locale_name ="";
     string description;
     string class_name;
     string sub_class_name;
     TemplateType type;
 public:
     string getName();
-    string getLocaleName() { return this->locale_name; }
+    string getLocaleName();
     string getDescription();
     string getClass();
     string getSubClass();
