6d8c6b83955dfaac15e3fe466105e7b49b22ee99
Tom <63907376+capt-t@users.noreply.github.com>
2020-05-02 20:00:00 +0200

    scenario_06: extract variable for faction

--- scripts/scenario_06_battlefield.lua
+++ scripts/scenario_06_battlefield.lua
@@ -35,24 +35,28 @@ function init()
         location_scale = 1
     end
 
+    local faction
+
+    faction = "Human Navy"
     for n = 1, 20 * battle_scale do
-        CpuShip():setTemplate("MT52 Hornet"):setPosition(random(-10000 * location_scale, 10000 * location_scale), random(0, 3000)):setRotation(90):setFaction("Human Navy"):orderRoaming():setScanned(true)
+        CpuShip():setTemplate("MT52 Hornet"):setPosition(random(-10000 * location_scale, 10000 * location_scale), random(0, 3000)):setRotation(90):setFaction(faction):orderRoaming():setScanned(true)
     end
     for n = 1, 10 * battle_scale do
-        CpuShip():setTemplate("Phobos T3"):setPosition(random(-10000 * location_scale, 10000 * location_scale), random(0, 2000)):setRotation(90):setFaction("Human Navy"):orderRoaming():setScanned(true)
+        CpuShip():setTemplate("Phobos T3"):setPosition(random(-10000 * location_scale, 10000 * location_scale), random(0, 2000)):setRotation(90):setFaction(faction):orderRoaming():setScanned(true)
     end
 
+    faction = "Exuari"
     for n = 1, 20 * battle_scale do
-        CpuShip():setTemplate("MT52 Hornet"):setPosition(random(-13000 * location_scale, 13000 * location_scale), random(5000, 8000)):setRotation(-90):setFaction("Exuari"):orderRoaming():setScanned(true)
+        CpuShip():setTemplate("MT52 Hornet"):setPosition(random(-13000 * location_scale, 13000 * location_scale), random(5000, 8000)):setRotation(-90):setFaction(faction):orderRoaming():setScanned(true)
     end
     for n = 1, 10 * battle_scale do
-        CpuShip():setTemplate("Phobos T3"):setPosition(random(-13000 * location_scale, 13000 * location_scale), random(5000, 8000)):setRotation(-90):setFaction("Exuari"):orderRoaming()
+        CpuShip():setTemplate("Phobos T3"):setPosition(random(-13000 * location_scale, 13000 * location_scale), random(5000, 8000)):setRotation(-90):setFaction(faction):orderRoaming()
     end
     for n = 1, 3 * battle_scale do
-        CpuShip():setTemplate("Piranha F12"):setPosition(random(-13000 * location_scale, 13000 * location_scale), 5000):setRotation(-90):setFaction("Exuari"):orderRoaming()
+        CpuShip():setTemplate("Piranha F12"):setPosition(random(-13000 * location_scale, 13000 * location_scale), 5000):setRotation(-90):setFaction(faction):orderRoaming()
     end
     for n = 1, 1 * battle_scale do
-        CpuShip():setTemplate("Atlantis X23"):setPosition(random(-3000 * location_scale, 3000 * location_scale), 7000):setRotation(-90):setFaction("Exuari"):orderRoaming()
+        CpuShip():setTemplate("Atlantis X23"):setPosition(random(-3000 * location_scale, 3000 * location_scale), 7000):setRotation(-90):setFaction(faction):orderRoaming()
     end
 end
 
