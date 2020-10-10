2f65e75d5be3b875354a49b4eae3cdedd4f434df
Tom <63907376+capt-t@users.noreply.github.com>
2020-10-10 20:00:00 +0200

    gameGlobalInfo: getSectorName

--- src/gameGlobalInfo.cpp
+++ src/gameGlobalInfo.cpp
@@ -246,6 +246,17 @@ string getSectorName(sf::Vector2f position)
     return y + x;
 }
 
+int getSectorName(lua_State* L)
+{
+    float x = luaL_checknumber(L, 1);
+    float y = luaL_checknumber(L, 2);
+    lua_pushstring(L, getSectorName(sf::Vector2f(x, y)).c_str());
+    return 1;
+}
+/// getSectorName(x, y)
+/// Return the sector name for the point with coordinates (x, y). Compare SpaceObject:getSectorName().
+REGISTER_SCRIPT_FUNCTION(getSectorName);
+
 static int victory(lua_State* L)
 {
     gameGlobalInfo->setVictory(luaL_checkstring(L, 1));
