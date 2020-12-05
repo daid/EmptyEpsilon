4addb4b1c279199b6a3a5065e875699ee97216df
Tom <63907376+capt-t@users.noreply.github.com>
2020-12-05 20:00:00 +0100

    luax: add table.equals, table.extend, table.clear

--- scripts/luax.lua
+++ scripts/luax.lua
@@ -6,11 +6,20 @@
 -- Only general functions that could be in the standard library
 -- are added to these modules.
 --
+-- Naming as in Lua's library: lowercase without underscores.
+-- Several functions are inspired by Python.
+--
 -- Require this module in scenario files or modules:
 --
 --     require("luax.lua")
 --
 -- **Version history**
+--
+-- Version 0.4
+--
+-- - add `table.equals`
+-- - add `table.extend`
+-- - add `table.clear`
 --
 -- Version 0.3
 --
@@ -44,6 +53,17 @@
 -- in the tables given as arguments, more precisely, they only
 -- consider the positions `1, ..., #list`.
 --
+-- Naming:
+--
+-- - The imperative `table.shuffle` operates in place,
+-- - the past participle `table.shuffled` would return a new list.
+-- - Third person `table.contains` returns a boolean.
+--
+-- Note:
+--
+-- - for `append` use `table.insert`
+-- - for `pop` use `table.remove`
+--
 -- @section table
 
 --- Checks whether a value is contained in a list.
@@ -54,13 +74,72 @@
 -- @tparam table list the list to search
 -- @tparam any value the value to look for
 -- @treturn boolean `true` if `value` is contained in `list`, otherwise `false`
+-- @usage
+-- table.contains({"a", "b"}, "a") -- true
+-- table.contains({"a", "b"}, "A") -- false
+-- table.contains({"a", "b", key = "c"}, "c") -- false
 function table.contains(list, value)
-    for pos = 1, #list do
-        if list[pos] == value then
+    for k = 1, #list do
+        if list[k] == value then
             return true
         end
     end
     return false
+end
+
+--- Return `true` if the two list are equal, otherwise `false`.
+--
+-- The lists are compared at position 1, 2, ... until both (or one) are `nil`.
+--
+-- Complexity: O(m), where m is the length of the shorter list.
+--
+-- @tparam table list1 the first list
+-- @tparam table list2 the second list
+-- @tparam ?function comp comparison function, default is `==`
+-- @treturn boolean result of comparison
+-- @usage
+-- {"a", "b"} == {"a", "b"} -- false
+-- table.equals({"a", "b"}, {"a", "b"}) -- true
+-- table.equals({"a", "b"}, {"a", "b", key = "c"}) -- true
+-- table.equals({"a", "b"}, {"b", "a"}) -- false
+function table.equals(list1, list2, comp)
+    if not comp then
+        comp = function(l1, l2)
+            return l1 == l2
+        end
+    end
+
+    if #list1 ~= #list2 then
+        return false
+    end
+
+    for k = 1, #list1 do
+        if list1[k] ~= list2[k] then
+            return false
+        end
+    end
+    return true
+end
+
+--- Extend the list `list` by appending all the items from `list2`.
+--
+-- As usual, non-list entries are maintained in `list` and ignored in `list2`.
+-- If `list` has integer keys greater than `#list`,
+-- they will simply be overwritten once these positions are reached.
+--
+-- Complexity: O(m), where m is the length of the second list.
+--
+-- @tparam table list the list to exend
+-- @tparam table list2 the list to be appended element by element to `list`
+-- @usage
+-- local list = {"a", "b", "c"}
+-- table.extend(list, {"D", "E"})
+-- assert(table.concat(list) == "abcDE")
+function table.extend(list, list2)
+    local k = #list
+    for j = 1, #list2 do
+        list[k + j] = list2[j]
+    end
 end
 
 --- Filters a list in place.
@@ -107,6 +186,35 @@ function table.filter(list, predicate)
     end
     for pos = pos_new, len_orig do
         list[pos] = nil
+    end
+end
+
+--- Remove all items from the list.
+--
+-- Use this function in particular if you have several references to the list.
+--
+-- As usual, non-list keys are ignored.
+-- This means, `#list` is 0 after `clear`, but there might still be other keys.
+--
+-- @tparam table list the list to clear
+-- @usage
+-- -- do
+-- local list = {"a", "b"}
+-- local ref = list
+-- print(#list, #ref) -- 2   2
+-- table.clear(list)
+-- print(#list, #ref) -- 0   0
+-- print(list == ref) -- true
+-- -- (usually) do not
+-- local list = {"a", "b"}
+-- local ref = list
+-- print(#list, #ref) -- 2   2
+-- list = {}
+-- print(#list, #ref) -- 0   2
+-- print(list == ref) -- false
+function table.clear(list)
+    for k = #list, 1, -1 do
+        list[k] = nil
     end
 end
 
