72c8cd1e97a08e6f2cf2b184678a48f9521fe7cc
David Priddy <priddys@yahoo.com>
2020-12-30 10:34:31 -0600

    Allow for supply freighter with a jump drive

--- scripts/supply_drop.lua
+++ scripts/supply_drop.lua
@@ -4,6 +4,9 @@
 
 function init()
 	my_ship = CpuShip():setCommsScript("comms_supply_drop.lua"):setFactionId(faction_id):setPosition(position_x, position_y):setTemplate("Flavia"):setScanned(true):orderFlyTowardsBlind(target_x, target_y)
+	if jump_freighter ~= nil and jump_freighter == "Yes" then
+		my_ship:setJumpDrive(true):setJumpDriveRange(5000,25000):setJumpDriveCharge(20000)
+	end
 	state = 0
 end
 
