542a3e6a2f17f2882fb01693a601a38f02995fc0
Oznogon <garrett+ozgh@oznogon.com>
2021-01-16 21:15:05 -0800

    Optional condensed GuiShieldsEnable with icon

--- src/screenComponents/shieldsEnableButton.h
+++ src/screenComponents/shieldsEnableButton.h
@@ -11,11 +11,14 @@ class GuiShieldsEnableButton : public GuiElement
 private:
     GuiToggleButton* button;
     GuiProgressbar* bar;
+    bool icon_only;
 public:
     GuiShieldsEnableButton(GuiContainer* owner, string id);
 
     virtual void onDraw(sf::RenderTarget& window) override;
     virtual void onHotkey(const HotkeyResult& key) override;
+
+    GuiShieldsEnableButton* showIconOnly(bool icon_only);
 };
 
 #endif//SHIELDS_ENABLE_BUTTON_H
