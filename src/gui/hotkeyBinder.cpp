fab2c0724a62c4c1f49d0f1584c812e0a9eb61ca
oznogon <garrett+ozgh@github.com>
2021-01-20 12:05:07 -0800

    Document GuiHotkeyBinder

--- src/gui/hotkeyBinder.cpp
+++ src/gui/hotkeyBinder.cpp
@@ -21,8 +21,10 @@ void GuiHotkeyBinder::onFocusLost()
 
 bool GuiHotkeyBinder::onKey(sf::Event::KeyEvent key, int unicode)
 {
+    // If the field has focus and any known key is pressed ...
     if (has_focus && key.code != sf::Keyboard::Unknown)
     {
+        // Don't bind hardcoded "back" keys.
         if (key.code == sf::Keyboard::Escape
             || key.code == sf::Keyboard::Home
             || key.code == sf::Keyboard::F1)
@@ -30,6 +32,8 @@ bool GuiHotkeyBinder::onKey(sf::Event::KeyEvent key, int unicode)
             text = "";
             return true;
         }
+
+        // Get the key's string name and display it.
         string key_name = hotkeys.getStringForKey(key.code);
 
         if (key_name.length() > 0) {
