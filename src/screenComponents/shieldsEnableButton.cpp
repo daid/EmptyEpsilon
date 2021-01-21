d96ac01649316b28cde051818e5ab675ab06a9c8
Oznogon <garrett+ozgh@oznogon.com>
2021-01-21 00:54:16 -0800

    Revert and use redundant string formatting for translation purposes

--- src/screenComponents/shieldsEnableButton.cpp
+++ src/screenComponents/shieldsEnableButton.cpp
@@ -14,23 +14,15 @@ GuiShieldsEnableButton::GuiShieldsEnableButton(GuiContainer* owner, string id)
 : GuiElement(owner, id),
   icon_only(false)
 {
-    // Recommended minimum button size with default text size: 130, 40
-
-    // Shield toggle button.
     button = new GuiToggleButton(this, id + "_BUTTON", "Shields: ON", [](bool value) {
         if (my_spaceship)
-        {
             my_spaceship->commandSetShields(!my_spaceship->shields_active);
-        }
     });
     button->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
-
-    // Progress bar during calibration.
     bar = new GuiProgressbar(this, id + "_BAR", 0.0, PlayerSpaceship::shield_calibration_time, 0);
     bar->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
-    (new GuiLabel(bar, id + "_CALIBRATING_LABEL", tr("shields", "Calibrating"), 30))->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
+    (new GuiLabel(bar, id + "_CALIBRATING_LABEL", tr("shields","Calibrating"), 30))->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
 
-    // Show power/damage indicator overlay.
     (new GuiPowerDamageIndicator(this, id + "_PDI", SYS_FrontShield, ACenterLeft))->setSize(GuiElement::GuiSizeMax, GuiElement::GuiSizeMax);
 }
 
@@ -40,40 +32,34 @@ void GuiShieldsEnableButton::onDraw(sf::RenderTarget& window)
     {
         if (my_spaceship->shield_calibration_delay > 0.0)
         {
-            // If the shields are calibrating, show a progress bar.
             button->hide();
             bar->show();
             bar->setValue(my_spaceship->shield_calibration_delay);
         }
         else
         {
-            // Show the button, hide the bar, set button state to shield activation state.
             button->show();
             button->setValue(my_spaceship->shields_active);
             bar->hide();
-
-            // Format strings based on settings and shield activation state.
-            string shield_frequency = icon_only ? frequencyToString(my_spaceship->shield_frequency) : tr("{frequency} Shields: ").format({{"frequency", frequencyToString(my_spaceship->shield_frequency)}});
-            string shield_status = my_spaceship->shields_active ? tr("shields", "ON") : tr("shields", "OFF");
-
-            // If the game's using beam frequencies, show the shield's current frequency setting.
-            // If the button's only showing the icon, don't bother with long, formatted strings.
+            string shield_status=my_spaceship->shields_active ? tr("shields","ON") : tr("shields","OFF");
             if (gameGlobalInfo->use_beam_shield_frequencies)
             {
                 if (icon_only)
                 {
-                    button->setText(shield_frequency);
+                    // Use compact string.
+                    button->setText(tr("{frequency}").format({{"frequency", frequencyToString(my_spaceship->shield_frequency)}}));
                 }
                 else
                 {
-                    button->setText(string("{frequency} {status}").format({{"frequency", shield_frequency}, {"status", shield_status}}));
+                    // Use long string.
+                    button->setText(tr("{frequency} Shields: {status}").format({{"frequency", frequencyToString(my_spaceship->shield_frequency)}, {"status", shield_status}}));
                 }
             }
             else
             {
                 if (icon_only)
                 {
-                    button->setText(shield_status);
+                    button->setText(tr("{status}").format({{"status", shield_status}}));
                 }
                 else
                 {
