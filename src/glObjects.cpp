92aff8e40f887184daa66f90a15ad768a1416818
gcask <53709079+gcask@users.noreply.github.com>
2021-02-15 12:03:28 -0500

    Particle instancing (#1310)
    * Particle instancing
    
    Using instancing technique for the particle system.
    
    The particle shader now supports instancing 64 particles per draw call.
    
    * Removed lambda auto parameter
    
    forgot this was not c++11.
    
    * Minor fixes
    
    - Isolating update body to rendering.
    - using array size instead of constexpr value to sidestep constexpr reference.
    
    * ES2 compliance
    
    - Fully converted the particle shader to ES2.
    - Fixed up my maths when computing instance count.
    - ScopedTexture RAII object for texture bindings
    
    * macOS compatibility
    
    - Reverted the shader to 120 (GL2)
    - Fixed up triangle winding.
    
    Co-authored-by: daid <daid303@gmail.com>

--- src/glObjects.cpp
+++ src/glObjects.cpp
@@ -30,6 +30,17 @@ namespace gl
         }
     }
 
+    ScopedBufferBinding::ScopedBufferBinding(uint32_t target, uint32_t buffer)
+        :target{target}, buffer{buffer}
+    {
+        glBindBuffer(target, buffer);
+    }
+
+    ScopedBufferBinding::~ScopedBufferBinding()
+    {
+        glBindBuffer(target, GL_NONE);
+    }
+
     ScopedVertexAttribArray::ScopedVertexAttribArray(int32_t attrib)
         :attrib{ attrib }
     {
@@ -41,6 +52,22 @@ namespace gl
     {
         if (attrib != -1)
             glDisableVertexAttribArray(attrib);
+    }
+
+    ScopedTexture::ScopedTexture(uint32_t target, uint32_t texture)
+        :target{ target }, texture{ texture }
+    {
+        // ES2 only supports 2D textures and cubemaps.
+        glGetIntegerv(target == GL_TEXTURE_2D ? GL_TEXTURE_BINDING_2D : GL_TEXTURE_BINDING_CUBE_MAP, &previously_bound);
+        
+        if (previously_bound != texture)
+            glBindTexture(target, texture);
+    }
+
+    ScopedTexture::~ScopedTexture()
+    {
+        if (previously_bound != texture)
+            glBindTexture(target, previously_bound);
     }
 
     bool isAvailable()
