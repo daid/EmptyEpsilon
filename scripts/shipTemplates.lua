cd4a1ece2eb3e09a21c47fe74924aec86fd93167
Pitlhit <pithlit@mail.de>
2021-05-07 17:38:15 +0200

    Add ship templates for the Exuari faction.

--- scripts/shipTemplates.lua
+++ scripts/shipTemplates.lua
@@ -8,15 +8,15 @@ These are:
 * Frigates: Medium sized ships. Operate on a small crew.
 * Corvette: Large, slower, less maneuverable ships.
 * Dreadnaught: Huge things. Everything in here is really really big, and generally really really deadly.
+* Exuari: Ships with a similar style, designed (but not limited) for the Exuari faction.
 
 Player ships are in general large frigates to small corvette class
 --]]
 require("shipTemplates_Stations.lua")
----[[Until these are ready, they are disabled
 require("shipTemplates_StarFighters.lua")
 require("shipTemplates_Frigates.lua")
 require("shipTemplates_Corvette.lua")
 require("shipTemplates_Dreadnaught.lua")
---]]
+require("shipTemplates_Exuari.lua")
 --For now, we add our old ship templates as well. These should be removed at some point.
 require("shipTemplates_OLD.lua")
