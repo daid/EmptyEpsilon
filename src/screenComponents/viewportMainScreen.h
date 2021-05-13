97da4f0b14a998f8bb867ef74c83840ab8529d9d
aBlueShadow <falter@mxdreamer>
2021-05-13 18:00:21 +0200

    options for mainscreen display flags

--- src/screenComponents/viewportMainScreen.h
+++ src/screenComponents/viewportMainScreen.h
@@ -11,6 +11,10 @@ public:
     virtual void onDraw(sf::RenderTarget& window);
 
     bool first_person = false;
+
+    constexpr static uint8_t flag_callsigns = 0x04;
+    constexpr static uint8_t flag_headings  = 0x02;
+    constexpr static uint8_t flag_spacedust = 0x01;
 };
 
 #endif//VIEWPORT_MAIN_SCREEN_H
