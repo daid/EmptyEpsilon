e276c305c6cfa988dc1368a3ab0bcbc7648e2178
muerteFR <61491656+muerteFR@users.noreply.github.com>
2021-05-17 13:43:54 +0200

    Grouping of some translation entries (#1418)
    * Grouping of some translation entries
    
    - Grouping of some translation entries to avoid unnecessary redundancies in .po files : button "Cancel", "Back", "Close"
    
    * Creation quick_basic.en.po and quick_basic.fr.po
    
    * I took the wrong branch :-)

--- src/menus/optionsMenu.cpp
+++ src/menus/optionsMenu.cpp
@@ -195,7 +195,7 @@ OptionsMenu::OptionsMenu()
 
     // Bottom GUI.
     // Back button.
-    (new GuiButton(this, "BACK", tr("options", "Back"), [this]()
+    (new GuiButton(this, "BACK", tr("button", "Back"), [this]()
     {
         // Close this menu, stop the music, and return to the main menu.
         destroy();
