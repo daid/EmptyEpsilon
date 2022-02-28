97414b79315f4feee602235ee9664942df970b5b
Daid <daid303@gmail.com>
2022-02-28 13:01:43 +0000

    Add back shiptemplate translations

--- update_locale.py
+++ update_locale.py
@@ -80,5 +80,6 @@ for script in glob.glob("scripts/**/*.lua", recursive=True):
     else:
         update_other_languages(output)
         print("Done %s" % (script))
+    print(info)
 
 update_other_languages("resources/locale/main.en.po")
