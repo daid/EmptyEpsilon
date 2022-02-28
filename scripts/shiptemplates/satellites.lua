467e8d39c3a59f1a9cf210b6add1be3d15e0be19
aBlueShadow <blue@shadow>
2022-02-28 20:32:38 +0100

    small template fix and de satellite translation

--- scripts/shiptemplates/satellites.lua
+++ scripts/shiptemplates/satellites.lua
@@ -1,4 +1,4 @@
-template = ShipTemplate():setName("ANT 615"):setModel("combatsat"):setClass(_("class", "Satellites"),_("subclass", "Sentry Series"))
+template = ShipTemplate():setName("ANT 615"):setLocaleName(_("ship", "ANT 615")):setModel("combatsat"):setClass(_("class", "Satellite"),_("subclass", "Sentry Series"))
 template:setDescription(_("Military satellite from the old days, back when the earth's population was much more divided than today. Its original purpose was probably to take out other satellites."))
 template:setRadarTrace("combatsat.png")
 --                 Arc,Dir,Range,CycleTime, Dmg
