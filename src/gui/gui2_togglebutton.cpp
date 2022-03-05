ef36bc60df93487e072f856ac565bd5a12d2436b
Daid <daid303@gmail.com>
2022-03-05 21:10:26 +0100

    Draw icons in togglebuttons

--- src/gui/gui2_togglebutton.cpp
+++ src/gui/gui2_togglebutton.cpp
@@ -15,8 +15,31 @@ void GuiToggleButton::onDraw(sp::RenderTarget& renderer)
 {
     const auto& back = value ? back_on_style->get(getState()) : back_style->get(getState());
     const auto& front = value ? front_on_style->get(getState()) : front_style->get(getState());
+
     renderer.drawStretched(rect, back.texture, back.color);
-    renderer.drawText(rect, text, sp::Alignment::Center, text_size > 0 ? text_size : front.size, front.font, front.color);
+    if (icon_name != "")
+    {
+        sp::Rect text_rect = rect;
+        sp::Alignment text_align = sp::Alignment::CenterLeft;
+        float icon_x;
+        switch(icon_alignment)
+        {
+        case sp::Alignment::CenterLeft:
+        case sp::Alignment::TopLeft:
+        case sp::Alignment::BottomLeft:
+            icon_x = rect.position.x + rect.size.y * 0.5f;
+            text_rect.position.x = rect.position.x + rect.size.y;
+            break;
+        default:
+            icon_x = rect.position.x + rect.size.x - rect.size.y * 0.5f;
+            text_rect.size.x = rect.size.x - rect.size.y;
+            text_align = sp::Alignment::CenterRight;
+        }
+        renderer.drawSprite(icon_name, glm::vec2(icon_x, rect.position.y + rect.size.y * 0.5f), rect.size.y * 0.8f, front.color);
+        renderer.drawText(text_rect, text, text_align, text_size > 0 ? text_size : front.size, front.font, front.color);
+    }else{
+        renderer.drawText(rect, text, sp::Alignment::Center, text_size > 0 ? text_size : front.size, front.font, front.color);
+    }
 }
 
 bool GuiToggleButton::getValue() const
