427946ddad78abccd36d5b981128aae1392282d4
Tomáš "hemmond" Látal <hemmond@hemmond.cz>
2022-08-19 01:14:20 +0200

    Added Warp Jammer range getter, hull getter and setter and some LUA bindings documentation to other files.

--- src/spaceObjects/spaceship.cpp
+++ src/spaceObjects/spaceship.cpp
@@ -34,6 +34,8 @@ REGISTER_SCRIPT_SUBCLASS_NO_CREATE(SpaceShip, ShipTemplateBasedObject)
     REGISTER_SCRIPT_CLASS_FUNCTION(SpaceShip, isDocked);
     REGISTER_SCRIPT_CLASS_FUNCTION(SpaceShip, getDockedWith);
     REGISTER_SCRIPT_CLASS_FUNCTION(SpaceShip, getDockingState);
+    /// Returns gets this ship target.
+    /// For example enemy targetted by player ship's weapons.
     REGISTER_SCRIPT_CLASS_FUNCTION(SpaceShip, getTarget);
     REGISTER_SCRIPT_CLASS_FUNCTION(SpaceShip, getWeaponStorage);
     REGISTER_SCRIPT_CLASS_FUNCTION(SpaceShip, getWeaponStorageMax);
