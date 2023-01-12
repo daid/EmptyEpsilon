79efeefbfd44bc198ee4aaa45210211b6c7b93c3
Garrett Guillotte <oznogon@users.noreply.github.com>
2023-01-12 00:33:32 -0800

    [scriptdocs] Edit and add SpaceStation docs (#1823)

--- src/spaceObjects/spaceStation.cpp
+++ src/spaceObjects/spaceStation.cpp
@@ -10,6 +10,14 @@
 #include "pathPlanner.h"
 
 #include "scriptInterface.h"
+
+/// A SpaceStation is an immobile ship-like object that repairs, resupplies, and recharges ships that dock with it.
+/// It sets several ShipTemplateBasedObject properties upon creation:
+/// - Its default callsign begins with "DS".
+/// - It restocks scan probes and CpuShip weapons by default.
+/// - It uses the scripts/comms_station.lua comms script by default.
+/// - When destroyed by damage, it awards or deducts a number of reputation points relative to its total shield strength and segments.
+/// - Any non-hostile SpaceShip can dock with it by default.
 REGISTER_SCRIPT_SUBCLASS(SpaceStation, ShipTemplateBasedObject)
 {
 }
