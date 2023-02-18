c570a38dd1a753783f43a488e2984d6ee06c6d72
muerteFR <dav.perrine@gmail.com>
2023-02-18 16:39:19 +0100

    Update scenario_57_shoreline.lua

--- scripts/scenario_57_shoreline.lua
+++ scripts/scenario_57_shoreline.lua
@@ -7520,7 +7520,7 @@ function handleDockedState()
 				addCommsReply("Back", commsStation)
 			end)
 		end
-		addCommsReply(_("explainGoods-comms", "No tutorial covered goods or cargo. Explain",) function()
+		addCommsReply(_("explainGoods-comms", "No tutorial covered goods or cargo. Explain"), function()
 			setCommsMessage(_("explainGoods-comms", "Different types of cargo or goods may be obtained from stations, freighters or other sources. They go by one word descriptions such as dilithium, optic, warp, etc. Certain mission goals may require a particular type or types of cargo. Each player ship differs in cargo carrying capacity. Goods may be obtained by spending reputation points or by trading other types of cargo (typically food, medicine or luxury)"))
 			addCommsReply("Back", commsStation)
 		end)
