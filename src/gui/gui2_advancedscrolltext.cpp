5cfbe9883b142b3e1fa5b8dd58050950056dead5
oznogon <oznogon@users.noreply.github.com>
2023-03-18 16:00:42 -0700

    [GuiAdvancedScrollText] Re-prep fonts when window width changes
    Re-prep fonts to reflow and recalculate entry height when the
    window width changes. This causes significant performance impacts
    when resizing the window with a large log.

--- src/gui/gui2_advancedscrolltext.cpp
+++ src/gui/gui2_advancedscrolltext.cpp
@@ -1,7 +1,7 @@
 #include "gui2_advancedscrolltext.h"
 
 GuiAdvancedScrollText::GuiAdvancedScrollText(GuiContainer* owner, string id)
-: GuiElement(owner, id), text_size(30.0f), max_prefix_width(0.0f)
+: GuiElement(owner, id), text_size(30.0f), rect_width(rect.size.x), max_prefix_width(0.0f)
 {
     scrollbar = new GuiScrollbar(this, id + "_SCROLL", 0, 1, 0, nullptr);
     scrollbar->setPosition(0, 0, sp::Alignment::TopRight)->setSize(50, GuiElement::GuiSizeMax);
@@ -48,11 +48,22 @@ GuiAdvancedScrollText* GuiAdvancedScrollText::clearEntries()
 
 void GuiAdvancedScrollText::onDraw(sp::RenderTarget& renderer)
 {
+    const bool is_resized = rect_width != rect.size.x;
+    if (is_resized) { rect_width = rect.size.x; }
+
     //Draw the visible entries
     float draw_offset = -scrollbar->getValue() + text_size + 12.0f;
 
     for(Entry& e : entries)
     {
+        if (is_resized)
+        {
+            // Window width has changed. Re-prep fonts.
+            e.prepared_prefix = sp::RenderTarget::getDefaultFont()->prepare(e.prefix, 32, text_size, rect.size, sp::Alignment::TopLeft);
+            max_prefix_width = std::max(max_prefix_width, e.prepared_prefix.getUsedAreaSize().x);
+            e.prepared_text = sp::RenderTarget::getDefaultFont()->prepare(e.text, 32, text_size, {rect.size.x - max_prefix_width - 50.0f, rect.size.y}, sp::Alignment::TopLeft, sp::Font::FlagLineWrap | sp::Font::FlagClip);
+        }
+
         const float height = e.prepared_text.getUsedAreaSize().y;
 
         if (draw_offset + height > 0
