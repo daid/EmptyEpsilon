070ca6a89f883208e94afb6f47f9e8e65a05a7f9
Chris Sibbitt <csibbitt@gmail.com>
2023-10-16 05:12:13 -0400

    [GuiAdvancedScrollText] Performance improvements (#2019)
    * [GuiAdvancedScrollText] Move prefix width calculation to addEntry
    
    * [GuiAdvancedScrollText] Disregard text that would be drawn below rect
    
    * [GuiAdvancedScrollText] Use unsigned ints for indexes
    
    * [GuiAdvancedScrollText] Move font prep from onDraw to addEntry
    
    Prepare fonts for entries only when adding an entry, rather than
    for every entry on every frame.
    
    * [GuiAdvancedScrollText] Re-prep fonts when window width changes
    
    Re-prep fonts to reflow and recalculate entry height when the
    window width changes. This causes significant performance impacts
    when resizing the window with a large log.
    
    * [GuiAdvancedScrollText] Track max_prefix_width when adding/removing
    
    * Refactored Entry font prep and max prefix tracking to its own method
    * Added a map for tracking prefix lengths
    * When removing Entries, update max_prefix_width if necesarry
    * Reset max_prefix_width (and tracking map) when resizing or clearing Entries
    
    * [GuiAdvancedScrollText] Fix the latest-line view on Relay startup
    
    * Needs to account for draw_offset alignment kludge
    
    * [GuiAdvancedScrollText][NO-OP] Simplify prepEntry signature
    
    * [GuiAdvancedScrollText] Fix bounds check in removeEntry
    
    * [GuiAdvancedScrollText] Revert to signed ints for indexes
    
    ---------
    
    Co-authored-by: oznogon <oznogon@users.noreply.github.com>

--- src/gui/gui2_advancedscrolltext.cpp
+++ src/gui/gui2_advancedscrolltext.cpp
@@ -1,7 +1,7 @@
 #include "gui2_advancedscrolltext.h"
 
 GuiAdvancedScrollText::GuiAdvancedScrollText(GuiContainer* owner, string id)
-: GuiElement(owner, id), text_size(30)
+: GuiElement(owner, id), text_size(30.0f), rect_width(rect.size.x), max_prefix_width(0.0f)
 {
     scrollbar = new GuiScrollbar(this, id + "_SCROLL", 0, 1, 0, nullptr);
     scrollbar->setPosition(0, 0, sp::Alignment::TopRight)->setSize(50, GuiElement::GuiSizeMax);
@@ -9,11 +9,12 @@ GuiAdvancedScrollText::GuiAdvancedScrollText(GuiContainer* owner, string id)
 
 GuiAdvancedScrollText* GuiAdvancedScrollText::addEntry(string prefix, string text, glm::u8vec4 color, unsigned int seq)
 {
-    entries.emplace_back();
-    entries.back().prefix = prefix;
-    entries.back().text = text;
-    entries.back().color = color;
-    entries.back().seq = seq;
+    Entry& entry = entries.emplace_back();
+    entry.prefix = prefix;
+    entry.text = text;
+    entry.color = color;
+    entry.seq = seq;
+    prepEntry(entry);
     return this;
 }
 
@@ -24,68 +25,102 @@ unsigned int GuiAdvancedScrollText::getEntryCount() const
 
 string GuiAdvancedScrollText::getEntryText(int index) const
 {
-    if (index < 0 || index >= int(getEntryCount()))
+    if (index < 0 || index >= static_cast<int>(getEntryCount()))
         return "";
     return entries[index].text;
 }
 
-unsigned int GuiAdvancedScrollText::getEntrySeq(unsigned int index) const
+GuiAdvancedScrollText::Entry GuiAdvancedScrollText::prepEntry(GuiAdvancedScrollText::Entry& e){
+    e.prepared_prefix = sp::RenderTarget::getDefaultFont()->prepare(e.prefix, 32, text_size, rect.size, sp::Alignment::TopLeft);
+    const float entry_prefix_width = e.prepared_prefix.getUsedAreaSize().x;
+    prefix_widths[entry_prefix_width] += 1;
+    max_prefix_width = std::max(max_prefix_width, entry_prefix_width);
+    e.prepared_text = sp::RenderTarget::getDefaultFont()->prepare(e.text, 32, text_size, {rect.size.x - max_prefix_width - 50.0f, rect.size.y}, sp::Alignment::TopLeft, sp::Font::FlagLineWrap | sp::Font::FlagClip);
+    return e;
+}
+
+unsigned int GuiAdvancedScrollText::getEntrySeq(int index) const
 {
-    if (index >= getEntryCount())
+    if (index < 0 || index >= static_cast<int>(getEntryCount()))
         return 0;
     return entries[index].seq;
 }
 
 GuiAdvancedScrollText* GuiAdvancedScrollText::removeEntry(int index)
 {
-    if (index < 0 || index > int(getEntryCount()))
+    if (index < 0 || index >= static_cast<int>(getEntryCount()))
         return this;
+
+    // Find new max prefix if entry was the last one with the current max
+    const float entry_prefix_width = entries[index].prepared_prefix.getUsedAreaSize().x;
+    bool last_with_width = false;
+    if(--prefix_widths[entry_prefix_width] == 0){
+        last_with_width = true;
+        prefix_widths.erase(entry_prefix_width);
+    }
+    if (entry_prefix_width == max_prefix_width && last_with_width){
+        max_prefix_width = prefix_widths.end()->first;
+    }
+
     entries.erase(entries.begin() + index);
+
     return this;
 }
 
 GuiAdvancedScrollText* GuiAdvancedScrollText::clearEntries()
 {
     entries.clear();
+    prefix_widths.clear();
+    max_prefix_width = 0;
     return this;
 }
 
 void GuiAdvancedScrollText::onDraw(sp::RenderTarget& renderer)
 {
-    //For all the entries, fix the maximum prefix width, so we know how much space we have for the text.
-    float max_prefix_width = 0.0f;
-    for(Entry& e : entries)
-    {
-        auto prepared = sp::RenderTarget::getDefaultFont()->prepare(e.prefix, 32, text_size, {0, 0}, sp::Alignment::TopLeft, 0);
-        max_prefix_width = std::max(max_prefix_width, prepared.getUsedAreaSize().x);
+    const bool is_resized = rect_width != rect.size.x;
+    if (is_resized) {
+        rect_width = rect.size.x;
+        prefix_widths.clear();
+        max_prefix_width = 0;
     }
 
     //Draw the visible entries
-    float draw_offset = -scrollbar->getValue();
+    float draw_offset = -scrollbar->getValue() + text_size + 12.0f;
+
     for(Entry& e : entries)
     {
-        auto prepared_prefix = sp::RenderTarget::getDefaultFont()->prepare(e.prefix, 32, text_size, rect.size, sp::Alignment::TopLeft);
-        auto prepared_text = sp::RenderTarget::getDefaultFont()->prepare(e.text, 32, text_size, {rect.size.x - max_prefix_width - 50, rect.size.y}, sp::Alignment::TopLeft, sp::Font::FlagLineWrap | sp::Font::FlagClip);
-        auto height = prepared_text.getUsedAreaSize().y;
-        if (draw_offset + height > 0)
+        // Window width has changed. Re-prep fonts.
+        if (is_resized){ prepEntry(e); }
+
+        const float height = e.prepared_text.getUsedAreaSize().y;
+
+        if (draw_offset + height > 0
+            && draw_offset < rect.size.y)
         {
-            for(auto& g : prepared_prefix.data)
-                g.position.y += draw_offset;
-            for(auto& g : prepared_text.data)
-                g.position.y += draw_offset;
-            renderer.drawText(rect, prepared_prefix, text_size, {255, 255, 255, 255}, sp::Font::FlagClip);
-            renderer.drawText(sp::Rect(rect.position.x + max_prefix_width, rect.position.y, rect.size.x - 50 - max_prefix_width, rect.size.y), prepared_text, text_size, e.color, sp::Font::FlagClip);
+            const float y_start = e.prepared_prefix.data[0].position.y;
+
+            for(auto& g : e.prepared_prefix.data)
+            {
+                g.position.y = draw_offset;
+            }
+            for(auto& g : e.prepared_text.data)
+            {
+                g.position.y = (g.position.y - y_start) + draw_offset;
+            }
+            renderer.drawText(rect, e.prepared_prefix, text_size, {255, 255, 255, 255}, sp::Font::FlagClip);
+            renderer.drawText(sp::Rect(rect.position.x + max_prefix_width, rect.position.y, rect.size.x - 50 - max_prefix_width, rect.size.y), e.prepared_text, text_size, e.color, sp::Font::FlagClip);
         }
+
         draw_offset += height;
     }
 
     //Calculate how many lines we have to display in total.
-    int line_count = draw_offset + scrollbar->getValue();
+    const int line_count = (draw_offset - text_size - 12.0f) + scrollbar->getValue();
 
     //Check if we need to update the scroll bar.
     if (scrollbar->getMax() != line_count)
     {
-        int diff = line_count - scrollbar->getMax();
+        const int diff = line_count - scrollbar->getMax();
         scrollbar->setRange(0, line_count);
         scrollbar->setValueSize(rect.size.y);
         if (auto_scroll_down)
