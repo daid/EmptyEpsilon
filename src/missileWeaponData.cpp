aa035d8d93a7df876c65613e9e417870da5250f5
Parracodea <parracodea@gmail.com>
2024-04-06 12:48:36 -0400

    Use Mine icon on Radar, instead of "blip" icon
    Mines also appear as the same color as Nukes (orange)

--- src/missileWeaponData.cpp
+++ src/missileWeaponData.cpp
@@ -5,7 +5,7 @@ MissileWeaponData missile_data[MW_Count] =
     //                speed, turnrate, lifetime, color, homing_range
     MissileWeaponData(200.0f, 10.f, 27.0f, glm::u8vec4(255, 200, 0, 255), 1200.0, "sfx/rlaunch.wav"),/*MW_Homing*/
     MissileWeaponData(200.0f, 10.f, 27.0f, glm::u8vec4(255, 100, 32, 255), 500.0, "sfx/rlaunch.wav"),/*MW_Nuke*/
-    MissileWeaponData(100.0f,  0.f, 10.0f, glm::u8vec4(255, 255, 255, 255), 0.0, "sfx/missile_launch.wav"),/*MW_Mine, lifetime is used at time which the mine is ejecting from the ship*/
+    MissileWeaponData(100.0f,  0.f, 10.0f, glm::u8vec4(255, 100, 32, 255), 0.0, "sfx/missile_launch.wav"),/*MW_Mine, lifetime is used at time which the mine is ejecting from the ship*/
     MissileWeaponData(200.0f, 10.f, 27.0f, glm::u8vec4(100, 32, 255, 255), 500.0, "sfx/rlaunch.wav"),/*MW_EMP*/
     MissileWeaponData(500.0f,  0.f, 13.5f, glm::u8vec4(200, 200, 200, 255), 0.0, "sfx/hvli_fire.wav"),/*MW_HVLI*/
 };
