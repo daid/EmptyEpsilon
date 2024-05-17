b12ec89f8760a1f4375bc2b70955e1e6b2da7171
Daid <daid303@gmail.com>
2024-05-17 16:05:22 +0200

    Working on multiplayer code

--- src/multiplayer/basic.h
+++ src/multiplayer/basic.h
@@ -4,7 +4,7 @@
 #include "ecs/query.h"
 
 namespace sp::io {
-    template<typename T> static inline DataBuffer& operator << (DataBuffer& packet, const std::vector<T>& v) { return packet << uint32_t(v.size()); for(size_t n=0; n<v.size(); n++) packet << v[n]; } \
+    template<typename T> static inline DataBuffer& operator << (DataBuffer& packet, const std::vector<T>& v) { packet << uint32_t(v.size()); for(size_t n=0; n<v.size(); n++) packet << v[n]; return packet;} \
     template<typename T> static inline DataBuffer& operator >> (DataBuffer& packet, std::vector<T>& v) { uint32_t size = 0; packet >> size; v.resize(size); for(size_t n=0; n<v.size(); n++) packet >> v[n]; return packet; }
 }
 
@@ -56,7 +56,7 @@ enum class BasicReplicationRequest {
     void CLASS::remove(sp::ecs::Entity entity) { entity.removeComponent<COMPONENT>(); } \
     template<BasicReplicationRequest BRR> void CLASS::impl(sp::ecs::Entity entity, sp::io::DataBuffer& packet, COMPONENT& target, COMPONENT* backup) { \
         sp::io::DataBuffer tmp; \
-        uint32_t flags; \
+        uint32_t flags = 0; \
         if (BRR == BasicReplicationRequest::Receive) packet >> flags; \
         field_impl<BRR>(entity, packet, target, backup, tmp, flags); \
         if (tmp.getDataSize() > 0) packet.write(CMD_ECS_SET_COMPONENT, component_index, entity.getIndex(), flags, tmp); \
@@ -68,9 +68,41 @@ enum class BasicReplicationRequest {
     switch(BRR) { \
     case BasicReplicationRequest::SendAll: flags |= flag; tmp << target.FIELD; break; \
     case BasicReplicationRequest::Update: if (target.FIELD != backup->FIELD) { flags |= flag; tmp << target.FIELD; backup->FIELD = target.FIELD; } break; \
-    case BasicReplicationRequest::Receive: if (flags & 1) packet >> target.FIELD; break; \
+    case BasicReplicationRequest::Receive: if (flags & flag) packet >> target.FIELD; break; \
     } \
     flag <<= 1;
+#define BASIC_REPLICATION_VECTOR(FIELD) \
+    switch(BRR) { \
+    case BasicReplicationRequest::SendAll: flags |= flag; tmp << target.FIELD.size(); break; \
+    case BasicReplicationRequest::Update: if (target.FIELD.size() != backup->FIELD.size()) { flags |= flag; tmp << target.FIELD.size(); backup->FIELD.resize(target.FIELD.size()); } break; \
+    case BasicReplicationRequest::Receive: if (flags & flag) { size_t size; packet >> size; target.FIELD.resize(size); } break; \
+    } \
+    flag <<= 1; \
+    for(size_t idx=0; idx<target.FIELD.size(); idx++) { \
+        uint32_t vector_flags = 0; \
+        if (BRR == BasicReplicationRequest::Receive) { \
+            packet >> vector_flags; \
+            if (vector_flags == 0) break; \
+            packet >> idx; \
+            if (idx >= target.FIELD.size()) break; \
+        } \
+        auto vector_target = &target.mounts[idx]; \
+        auto vector_backup = &backup->mounts[idx]; \
+        sp::io::DataBuffer vector_tmp; \
+        uint32_t vector_flag = 0;
+
+#define VECTOR_REPLICATION_FIELD(FIELD) \
+        switch(BRR) { \
+        case BasicReplicationRequest::SendAll: vector_flags |= vector_flag; vector_tmp << vector_target->FIELD; break; \
+        case BasicReplicationRequest::Update: if (vector_target->FIELD != vector_backup->FIELD) { vector_flags |= vector_flag; vector_tmp << vector_target->FIELD; vector_backup->FIELD = vector_target->FIELD; } break; \
+        case BasicReplicationRequest::Receive: if (vector_flags & vector_flag) packet >> vector_target->FIELD; break; \
+        } \
+        vector_flag <<= 1;
+
+#define VECTOR_REPLICATION_END() \
+        if (vector_tmp.getDataSize() > 0) tmp.write(flags, idx, vector_tmp); \
+    } \
+    if (tmp.getDataSize() > 0) tmp.write(uint32_t(0)); // end of vector update.
 
 
 
@@ -108,6 +140,6 @@ enum class BasicReplicationRequest {
     switch(BRR) { \
     case BasicReplicationRequest::SendAll: flags |= flag; tmp << target.VECTOR; break; \
     case BasicReplicationRequest::Update: if (target.DIRTY) { flags |= flag; tmp << target.VECTOR; target.DIRTY = false; } break; \
-    case BasicReplicationRequest::Receive: if (flags & 1) packet >> target.VECTOR; break; \
+    case BasicReplicationRequest::Receive: if (flags & flag) packet >> target.VECTOR; break; \
     } \
     flag <<= 1;
--- src/multiplayer/beamweapon.cpp
+++ src/multiplayer/beamweapon.cpp
@@ -1,4 +1,43 @@
 #include "multiplayer/beamweapon.h"
+#include "multiplayer.h"
+
+BASIC_REPLICATION_IMPL(BeamWeaponSysReplication, BeamWeaponSys)
+    BASIC_REPLICATION_FIELD(health);
+    BASIC_REPLICATION_FIELD(health_max);
+    BASIC_REPLICATION_FIELD(power_level);
+    BASIC_REPLICATION_FIELD(power_request);
+    BASIC_REPLICATION_FIELD(heat_level);
+    BASIC_REPLICATION_FIELD(coolant_level);
+    BASIC_REPLICATION_FIELD(coolant_request);
+    BASIC_REPLICATION_FIELD(can_be_hacked);
+    BASIC_REPLICATION_FIELD(hacked_level);
+    BASIC_REPLICATION_FIELD(power_factor);
+    BASIC_REPLICATION_FIELD(coolant_change_rate_per_second);
+    BASIC_REPLICATION_FIELD(heat_add_rate_per_second);
+    BASIC_REPLICATION_FIELD(power_change_rate_per_second);
+    BASIC_REPLICATION_FIELD(auto_repair_per_second);
+    BASIC_REPLICATION_FIELD(frequency);
+    BASIC_REPLICATION_FIELD(system_target);
+
+    BASIC_REPLICATION_VECTOR(mounts)
+        VECTOR_REPLICATION_FIELD(position);
+        VECTOR_REPLICATION_FIELD(arc);
+        VECTOR_REPLICATION_FIELD(direction);
+        VECTOR_REPLICATION_FIELD(range);
+        VECTOR_REPLICATION_FIELD(turret_arc);
+        VECTOR_REPLICATION_FIELD(turret_direction);
+        VECTOR_REPLICATION_FIELD(turret_rotation_rate);
+        VECTOR_REPLICATION_FIELD(cycle_time);
+        VECTOR_REPLICATION_FIELD(damage);
+        VECTOR_REPLICATION_FIELD(energy_per_beam_fire);
+        VECTOR_REPLICATION_FIELD(heat_per_beam_fire);
+        VECTOR_REPLICATION_FIELD(arc_color);
+        VECTOR_REPLICATION_FIELD(arc_color_fire);
+        VECTOR_REPLICATION_FIELD(damage_type);
+        VECTOR_REPLICATION_FIELD(cooldown);
+        VECTOR_REPLICATION_FIELD(texture);
+    VECTOR_REPLICATION_END();
+}
 
 
 BASIC_REPLICATION_IMPL(BeamEffectReplication, BeamEffect)
