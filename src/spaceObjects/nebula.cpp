9509451f83c81efbd4073b5907b55f9c51c4adb3
ragerin <ragerin@gmail.com>
2024-07-26 14:32:51 +0200

    improve rendering logic of nebula

--- src/spaceObjects/nebula.cpp
+++ src/spaceObjects/nebula.cpp
@@ -85,11 +85,11 @@ void Nebula::draw3DTransparent()
         float size = cloud.size;
 
         float distance = glm::length(camera_position - position);
-        float alpha = distance / 10000.0f;
+        float alpha = distance / 10000.0f;  // Linearly scale alpha based on distance
 
-        if (alpha < 0.0f)
+        if (alpha < 0.0f)       // Skip clouds if their alpha is below 0
             continue;
-        if (alpha > 1.0f)
+        if (alpha > 1.0f)       // Clamp alpha to 1.0 for clouds further away than the limit
             alpha = 1.0f;
 
         // Setup quad vertices with the current position
@@ -100,7 +100,7 @@ void Nebula::draw3DTransparent()
 
         textureManager.getTexture("Nebula" + string(cloud.texture) + ".png")->bind();
         // Update uniforms for each cloud
-        glUniform4f(shader.get().uniform(ShaderRegistry::Uniforms::Color), alpha * 0.8f, alpha * 0.8f, alpha * 0.8f, size);
+        glUniform4f(shader.get().uniform(ShaderRegistry::Uniforms::Color), alpha, alpha, alpha, size);
         auto model_matrix = glm::translate(getModelMatrix(), {cloud.offset.x, cloud.offset.y, 0});
         glUniformMatrix4fv(shader.get().uniform(ShaderRegistry::Uniforms::Model), 1, GL_FALSE, glm::value_ptr(model_matrix));
 
@@ -112,52 +112,6 @@ void Nebula::draw3DTransparent()
     glBlendFunc(GL_ONE, GL_ONE);
     glBlendEquation(GL_FUNC_ADD);
 }
-
-
-// void Nebula::draw3DTransparent()
-// {
-//     ShaderRegistry::ScopedShader shader(ShaderRegistry::Shaders::Billboard);
-
-//     std::array<VertexAndTexCoords, 4> quad{
-//         glm::vec3{}, {0.f, 1.f},
-//         glm::vec3{}, {1.f, 1.f},
-//         glm::vec3{}, {1.f, 0.f},
-//         glm::vec3{}, {0.f, 0.f}
-//     };
-
-//     gl::ScopedVertexAttribArray positions(shader.get().attribute(ShaderRegistry::Attributes::Position));
-//     gl::ScopedVertexAttribArray texcoords(shader.get().attribute(ShaderRegistry::Attributes::Texcoords));
-
-//     for(int n=0; n<cloud_count; n++)
-//     {
-//         NebulaCloud& cloud = clouds[n];
-
-//         glm::vec3 position = glm::vec3(getPosition().x, getPosition().y, 0) + glm::vec3(cloud.offset.x, cloud.offset.y, 0);
-//         float size = cloud.size;
-
-//         float distance = glm::length(camera_position - position);
-//         float alpha = distance / 10000.0f;
-//         // float alpha = 1.0f - (distance / 10000.0f);
-//         if (alpha < 0.0f)
-//             continue;
-
-//         // setup our quad.
-//         for (auto& point : quad)
-//         {
-//             point.vertex = position;
-//         }
-
-//         textureManager.getTexture("Nebula" + string(cloud.texture) + ".png")->bind();
-//         glUniform4f(shader.get().uniform(ShaderRegistry::Uniforms::Color), alpha * 0.8f, alpha * 0.8f, alpha * 0.8f, size);
-//         auto model_matrix = glm::translate(getModelMatrix(), {cloud.offset.x, cloud.offset.y, 0});
-//         glUniformMatrix4fv(shader.get().uniform(ShaderRegistry::Uniforms::Model), 1, GL_FALSE, glm::value_ptr(model_matrix));
-
-//         glVertexAttribPointer(positions.get(), 3, GL_FLOAT, GL_FALSE, sizeof(VertexAndTexCoords), (GLvoid*)quad.data());
-//         glVertexAttribPointer(texcoords.get(), 2, GL_FLOAT, GL_FALSE, sizeof(VertexAndTexCoords), (GLvoid*)((char*)quad.data() + sizeof(glm::vec3)));
-//         std::initializer_list<uint16_t> indices = { 0, 3, 2, 0, 2, 1 };
-//         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, std::begin(indices));
-//     }
-// }
 
 void Nebula::drawOnRadar(sp::RenderTarget& renderer, glm::vec2 position, float scale, float rotation, bool long_range)
 {
