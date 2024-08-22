8af301cfd5a71b284a85cae28b8cb6546fed1d68
GinjaNinja32 <ginjaninja32@gmail.com>
2024-08-23 00:00:21 +0100

    For server_scenario and headless, allow passing scenario_settings too

--- src/main.cpp
+++ src/main.cpp
@@ -420,7 +420,7 @@ int main(int argc, char** argv)
             return 1;
 
         // Load the scenario and open the ship selection screen.
-        gameGlobalInfo->startScenario(PreferencesManager::get("server_scenario"));
+        gameGlobalInfo->startScenario(PreferencesManager::get("server_scenario"), loadScenarioSettingsFromPrefs());
         new ShipSelectionScreen();
     }
 
@@ -475,7 +475,7 @@ void returnToMainMenu(RenderLayer* render_layer)
         if (PreferencesManager::get("headless_name") != "") game_server->setServerName(PreferencesManager::get("headless_name"));
         if (PreferencesManager::get("headless_password") != "") game_server->setPassword(PreferencesManager::get("headless_password").upper());
         if (PreferencesManager::get("headless_internet") == "1") game_server->registerOnMasterServer(PreferencesManager::get("registry_registration_url", "http://daid.eu/ee/register.php"));
-        gameGlobalInfo->startScenario(PreferencesManager::get("headless"));
+        gameGlobalInfo->startScenario(PreferencesManager::get("headless"), loadScenarioSettingsFromPrefs());
 
         if (PreferencesManager::get("startpaused") != "1")
             engine->setGameSpeed(1.0);
@@ -518,4 +518,26 @@ void returnToShipSelection(RenderLayer* render_layer)
 void returnToOptionMenu()
 {
     new OptionsMenu();
+}
+
+std::unordered_map<string, string> loadScenarioSettingsFromPrefs()
+{
+    string preferenceValue = PreferencesManager::get("scenario_settings");
+
+    std::unordered_map<string, string> settings = {};
+    if (preferenceValue == "")
+        return settings;
+
+    for(string setting : preferenceValue.split(";"))
+    {
+        std::vector<string> key_value = setting.split("=", 1);
+        string key = key_value[0].strip();
+        if (key.length() < 1)
+            continue;
+
+        if (key_value.size() == 2)
+            settings[key] = key_value[1];
+    }
+
+    return settings;
 }
