bb813a7bcde6bd08b7e4e663f55fb3c88d1c05d0
GinjaNinja32 <ginjaninja32@gmail.com>
2025-06-09 21:49:12 +0100

    Require impulse==0 for warp drive usage, rather than 100%

--- src/systems/impulse.cpp
+++ src/systems/impulse.cpp
@@ -24,7 +24,7 @@ void ImpulseSystem::update(float delta)
 
         auto request = std::clamp(impulse.request, -1.0f, 1.0f);
         if (warp_drive && (warp_drive->request > 0 || warp_drive->current > 0))
-            request = 1.0f;
+            request = 0.0f;
         if (impulse.actual < request)
         {
             if (cap_speed > 0)
