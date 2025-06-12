08f7b6102d868b197452ddf962bd5099fd0eda6d
GinjaNinja32 <ginjaninja32@gmail.com>
2025-06-12 02:25:45 +0100

    fix exponential callsign generation

--- scripts/api/callsign.lua
+++ scripts/api/callsign.lua
@@ -4,24 +4,24 @@ __random_callsign_prefix_length = 0
 __random_callsign_prefix_pool = {}
 function generateRandomCallSign(prefix)
     if prefix == nil then
-        if __random_callsign_prefix_length == 0 then
+        if #__random_callsign_prefix_pool < 1 then
+            __random_callsign_prefix_length = __random_callsign_prefix_length + 1
             for i=1,26 do
                 table.insert(__random_callsign_prefix_pool, string.char(i+64))
             end
-            __random_callsign_prefix_length = 1
-        end
-        for i=1,__random_callsign_prefix_length do
-            if #__random_callsign_prefix_pool < 1 then
-                for i=1,26 do
-                    table.insert(__random_callsign_prefix_pool, string.char(i+64))
+            for i = 2, __random_callsign_prefix_length do
+                local new_pool = {}
+                for _, a in ipairs(__random_callsign_prefix_pool) do
+                    for i=1,26 do
+                        table.insert(new_pool, a .. string.char(i+64))
+                    end
                 end
-                __random_callsign_prefix_length = __random_callsign_prefix_length + 1
+                __random_callsign_prefix_pool = new_pool
             end
-            local prefix_index = math.random(1,#__random_callsign_prefix_pool)
-            prefix = ""
-            prefix = prefix .. __random_callsign_prefix_pool[prefix_index]
-            table.remove(__random_callsign_prefix_pool, prefix_index)
         end
+        local prefix_index = math.random(1,#__random_callsign_prefix_pool)
+        prefix = __random_callsign_prefix_pool[prefix_index]
+        table.remove(__random_callsign_prefix_pool, prefix_index)
     end
     __random_callsign_index = __random_callsign_index + irandom(1, 3)
     if __random_callsign_index > 999 then
