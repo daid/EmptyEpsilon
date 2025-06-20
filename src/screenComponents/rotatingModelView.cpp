812398c5650e0fdfd7bad88af347c73217a90c68
Daid <daid303@gmail.com>
2025-06-20 10:26:02 +0200

    Fix the probe mesh being rendered really small and clean up the rotating model view code a bit.

--- src/screenComponents/rotatingModelView.cpp
+++ src/screenComponents/rotatingModelView.cpp
@@ -50,18 +50,17 @@ void GuiRotatingModelView::onDraw(sp::RenderTarget& renderer)
     glFrontFace(GL_CCW);
 
 
-    auto mesh_radius = mrc->getMesh()->greatest_distance_from_center;
+    auto mesh_radius = mrc->getMesh()->greatest_distance_from_center * mrc->scale;
     float mesh_diameter = mesh_radius * 2.f;
     float near_clip_boundary = 1.f;
 
     auto projection_matrix = glm::perspective(glm::radians(camera_fov), rect.size.x / rect.size.y, near_clip_boundary, 25000.f);
-    float max_size = mesh_diameter * glm::tan(glm::radians(camera_fov / 2.f));
-    float scale = max_size / mesh_diameter;
+    float view_distance = mesh_diameter / glm::tan(glm::radians(camera_fov / 2.f));
 
     // OpenGL standard: X across (left-to-right), Y up, Z "towards".
     auto view_matrix = glm::rotate(glm::identity<glm::mat4>(), glm::radians(90.f), glm::vec3(1.f, 0.f, 0.f)); // -> X across (l-t-r), Y "towards", Z down
     view_matrix = glm::scale(view_matrix, glm::vec3(1.f, 1.f, -1.f)); // -> X across (l-t-r), Y "towards", Z up
-    view_matrix = glm::translate(view_matrix, glm::vec3(0.f, -1.f * max_size - near_clip_boundary, 0.f));
+    view_matrix = glm::translate(view_matrix, glm::vec3(0.f, -1.f * view_distance - near_clip_boundary, 0.f));
     view_matrix = glm::rotate(view_matrix, glm::radians(-30.f), glm::vec3(1.f, 0.f, 0.f));
     view_matrix = glm::rotate(view_matrix, glm::radians(engine->getElapsedTime() * 360.0f / 10.0f), glm::vec3(0.f, 0.f, 1.f));
 
@@ -71,13 +70,13 @@ void GuiRotatingModelView::onDraw(sp::RenderTarget& renderer)
 
     ShaderRegistry::updateProjectionView(projection_matrix, view_matrix);
 
-    auto model_matrix = calculateModelMatrix(glm::vec2{}, 0.f, mrc->mesh_offset, scale);
+    auto model_matrix = calculateModelMatrix(glm::vec2{}, 0.f, mrc->mesh_offset, mrc->scale);
 
     auto shader = lookUpShader(*mrc);
     glUniformMatrix4fv(shader.get().uniform(ShaderRegistry::Uniforms::Model), 1, GL_FALSE, glm::value_ptr(model_matrix));
 
     auto modeldata_matrix = glm::rotate(model_matrix, glm::radians(180.f), {0.f, 0.f, 1.f});
-    modeldata_matrix = glm::scale(modeldata_matrix, glm::vec3{scale});
+    modeldata_matrix = glm::scale(modeldata_matrix, glm::vec3{mrc->scale});
 
     // Lights setup.
     ShaderRegistry::setupLights(shader.get(), modeldata_matrix);
@@ -86,76 +85,29 @@ void GuiRotatingModelView::onDraw(sp::RenderTarget& renderer)
     activateAndBindMeshTextures(*mrc);
 
     // Draw
-
-//     {
-// #ifdef DEBUG
-//         ShaderRegistry::ScopedShader shader(ShaderRegistry::Shaders::BasicColor);
-//         {
-//             // Common state - matrices.
-//             glUniformMatrix4fv(shader.get().uniform(ShaderRegistry::Uniforms::Projection), 1, GL_FALSE, glm::value_ptr(projection_matrix));
-//             glUniformMatrix4fv(shader.get().uniform(ShaderRegistry::Uniforms::View), 1, GL_FALSE, glm::value_ptr(view_matrix));
-//             glUniformMatrix4fv(shader.get().uniform(ShaderRegistry::Uniforms::Model), 1, GL_FALSE, glm::value_ptr(model_matrix));
-//
-//             // Vertex attrib
-//             gl::ScopedVertexAttribArray positions(shader.get().attribute(ShaderRegistry::Attributes::Position));
-//
-//             // for (const EngineEmitterData& ee : model->engine_emitters)
-//             // {
-//             //     glm::vec3 offset = ee.position * model->scale;
-//             //     float r = model->scale * ee.scale * 0.5f;
-//             //
-//             //     glUniform4f(shader.get().uniform(ShaderRegistry::Uniforms::Color), ee.color.x, ee.color.y, ee.color.z, 1.f);
-//             //     auto vertices = {
-//             //         glm::vec3{offset.x + r, offset.y, offset.z},
-//             //         glm::vec3{offset.x - r, offset.y, offset.z},
-//             //         glm::vec3{offset.x, offset.y + r, offset.z},
-//             //         glm::vec3{offset.x, offset.y - r, offset.z},
-//             //         glm::vec3{offset.x, offset.y, offset.z + r},
-//             //         glm::vec3{offset.x, offset.y, offset.z - r}
-//             //     };
-//             //     glVertexAttribPointer(positions.get(), 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), std::begin(vertices));
-//             //     glDrawArrays(GL_LINES, 0, vertices.size());
-//             // }
-//             auto r = mesh_radius * 0.1f;
-//             // float r = model->getRadius() * 0.1f;
-//
-//             glUniform4f(shader.get().uniform(ShaderRegistry::Uniforms::Color), 1.f, 1.f, 1.f, 1.f);
-//
-//             // for (const glm::vec3& position : model->beam_position)
-//             // {
-//             //     glm::vec3 offset = position * model->scale;
-//             //
-//             //     auto vertices = {
-//             //         glm::vec3{offset.x + r, offset.y, offset.z},
-//             //         glm::vec3{offset.x - r, offset.y, offset.z},
-//             //         glm::vec3{offset.x, offset.y + r, offset.z},
-//             //         glm::vec3{offset.x, offset.y - r, offset.z},
-//             //         glm::vec3{offset.x, offset.y, offset.z + r},
-//             //         glm::vec3{offset.x, offset.y, offset.z - r}
-//             //     };
-//             //     glVertexAttribPointer(positions.get(), 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), std::begin(vertices));
-//             //     glDrawArrays(GL_LINES, 0, vertices.size());
-//             // }
-//
-//             // for (const glm::vec3& position : model->tube_position)
-//             // {
-//             //     glm::vec3 offset = position * model->scale;
-//             //
-//             //     auto vertices = {
-//             //         glm::vec3{offset.x + r * 3, offset.y, offset.z},
-//             //         glm::vec3{offset.x - r, offset.y, offset.z},
-//             //         glm::vec3{offset.x, offset.y + r, offset.z},
-//             //         glm::vec3{offset.x, offset.y - r, offset.z},
-//             //         glm::vec3{offset.x, offset.y, offset.z + r},
-//             //         glm::vec3{offset.x, offset.y, offset.z - r}
-//             //     };
-//             //     glVertexAttribPointer(positions.get(), 3, GL_FLOAT, GL_FALSE, sizeof(glm::vec3), std::begin(vertices));
-//             //     glDrawArrays(GL_LINES, 0, vertices.size());
-//             // }
-//         }
-// #endif
-//     }
     drawMesh(*mrc, shader);
+
+#if 0
+    {
+        ShaderRegistry::ScopedShader color_shader(ShaderRegistry::Shaders::BasicColor);
+        glDisable(GL_DEPTH_TEST);
+        auto model_matrix = glm::identity<glm::mat4>();
+        glUniformMatrix4fv(color_shader.get().uniform(ShaderRegistry::Uniforms::Model), 1, GL_FALSE, glm::value_ptr(model_matrix));
+        glUniform4f(color_shader.get().uniform(ShaderRegistry::Uniforms::Color), 1.0f, 1.0f, 1.0f, .5f);
+        gl::ScopedVertexAttribArray positions(color_shader.get().attribute(ShaderRegistry::Attributes::Position));
+        constexpr size_t point_count = 50;
+        std::vector<glm::vec3> vertices;
+        std::vector<uint16_t> indices;
+        auto radius = mesh_radius;
+        for(size_t idx=0; idx<point_count; idx++) {
+            float f = float(idx) / float(point_count) * static_cast<float>(M_PI) * 2.0f;
+            vertices.push_back({std::sin(f) * radius, std::cos(f) * radius, 0.0f});
+            indices.push_back(idx);
+        }
+        glVertexAttribPointer(positions.get(), 3, GL_FLOAT, GL_FALSE, 0, reinterpret_cast<GLvoid*>(&vertices[0]));
+        glDrawElements(GL_LINE_LOOP, point_count, GL_UNSIGNED_SHORT, reinterpret_cast<GLvoid*>(&indices[0]));
+    }
+#endif
 
     glDisable(GL_CULL_FACE);
     glDisable(GL_DEPTH_TEST);
