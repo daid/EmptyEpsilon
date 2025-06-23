8e9d549766dde3b210f3a83d70474bb9e6ae2072
GinjaNinja32 <ginjaninja32@gmail.com>
2025-06-23 23:29:25 +0100

    Add missing replication for Zone.skybox

--- src/multiplayer/zone.cpp
+++ src/multiplayer/zone.cpp
@@ -6,6 +6,7 @@ BASIC_REPLICATION_IMPL(ZoneReplication, Zone)
     BASIC_REPLICATION_FIELD(color);
     BASIC_REPLICATION_FIELD(label);
     BASIC_REPLICATION_FIELD(label_offset);
+    BASIC_REPLICATION_FIELD(skybox);
     REPLICATE_VECTOR_IF_DIRTY(outline, zone_dirty);
     if (BRR == BasicReplicationRequest::Receive && (flags & (flag >> 1))) target.updateTriangles();
 }
