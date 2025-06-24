96a66294e1afa87beedf4d61ff4b515b42e3f4a1
GinjaNinja32 <ginjaninja32@gmail.com>
2025-06-24 20:04:56 +0100

    Add missing default skybox fade distance

--- src/components/zone.h
+++ src/components/zone.h
@@ -14,7 +14,7 @@ public:
     string label;
     glm::vec2 label_offset;
     string skybox;
-    float skybox_fade_distance; // distance from edge of zone for skybox to fully fade in
+    float skybox_fade_distance = 0.0f; // distance from edge of zone for skybox to fully fade in
     float radius;
     bool zone_dirty = true;
 
