6ad6abf014707d62ceb4cc624412e85b09a0b291
T <hillhand@gmail.com>
2025-06-25 03:19:59 -0700

    Update nebula.lua
    Removed commented out code and a few half-baked ideas

--- scripts/api/entity/nebula.lua
+++ scripts/api/entity/nebula.lua
@@ -5,33 +5,12 @@
 --- In 3D space, a Nebula resembles a dense cloud of colorful gases.
 --- Example: nebula = Nebula():setPosition(1000,2000)
 --- @type creation
--- function Nebula(radius)
-    -- radius = radius or 5000.0
-    -- local e = createEntity()
-    -- e.components.radar_signature = {gravity=0, electrical=0.8, biological=-1.0}
-    -- e.components.transform = {rotation=random(0, 360)}
-    -- e.components.radar_trace = {icon="Nebula" .. irandom(1, 3) .. ".png", min_size=16, max_size=nil, radius=radius*1.5, blend_add=true}
-    -- e.components.radar_block = {range=radius}
-    -- e.components.never_radar_blocked = {}
-    -- local render_info = {}
-    -- local cloud_count = 32
-    -- for n=1,cloud_count do
-        -- local size = random(1024, 1024^1.5)
-        -- local dist = random(size / 2.0, radius - size)
-        -- local angle = n * 360 / cloud_count
-        -- local ox, oy = math.cos(angle / 180 * math.pi) * dist, math.sin(angle / 180 * math.pi) * dist
-        -- render_info[n] = {size=size, texture="Nebula" .. irandom(1, 3) .. ".png", offset={ox, oy}}
-    -- end
-    -- e.components.nebula_renderer = render_info
-    -- return e
--- end
-
 function Nebula(radius)
-    local radius = radius or 5000.0
+    local radius = 5000.0
     local e = createEntity()
     e.components.radar_signature = {gravity=0, electrical=0.8, biological=-1.0}
     e.components.transform = {rotation=random(0, 360)}
-    e.components.radar_trace = {icon="radar/nebula" .. irandom(1, 4) .. ".png", min_size=0, max_size = 9999999, radius=radius*1.15, blend_add=true}
+    e.components.radar_trace = {icon="radar/nebula.png" .. irandom(1, 4) .. ".png", min_size=0, max_size = 9999999, radius=radius*1.15, blend_add=true}
     e.components.radar_block = {range=radius}
     e.components.never_radar_blocked = {}
     
