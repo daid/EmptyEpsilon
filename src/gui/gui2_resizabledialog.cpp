32f6694ee1856ee356a65d95289248a71399ad75
Garrett Guillotte <oznogon@users.noreply.github.com>
2025-10-20 23:11:23 -0700

    Refactor GuiResizableDialog drag-to-reposition (#2554)
    - Fix positional delta tracking on drag
    - Enforce limits on drag to reduce flickering near window edges
    - Reduce tolerances that led to title bar exiting window bounds

--- src/gui/gui2_resizabledialog.cpp
+++ src/gui/gui2_resizabledialog.cpp
@@ -63,18 +63,18 @@ void GuiResizableDialog::onDraw(sp::RenderTarget& renderer)
     GuiPanel::onDraw(renderer);
 
     contents->setSize(rect.size.x - resize_icon_size, rect.size.y - title_bar_height - resize_icon_size / 2.0f);
+    window_size = renderer.getVirtualSize();
 
-    if (rect.position.x < -50)
-        setPosition(-50, getPositionOffset().y);
+    if (rect.position.x < -50.0f)
+        setPosition(-50.0f, getPositionOffset().y);
     if (rect.position.y < -(title_bar_height / 2.0f))
         setPosition(getPositionOffset().x, -(title_bar_height / 2.0f));
-    if (rect.position.x > renderer.getVirtualSize().x - 50)
-        setPosition(renderer.getVirtualSize().x - 50, getPositionOffset().y);
-    if (rect.position.y > renderer.getVirtualSize().y - title_bar_height)
-        setPosition(getPositionOffset().x, renderer.getVirtualSize().y - title_bar_height);
+    if (rect.position.x > window_size.x - 50.0f)
+        setPosition(window_size.x - 50.0f, getPositionOffset().y);
+    if (rect.position.y > window_size.y - title_bar_height)
+        setPosition(getPositionOffset().x, window_size.y - title_bar_height);
 
-    if (minimized)
-        return;
+    if (minimized) return;
 
     renderer.drawSprite("gui/widget/ResizeDialogCorner.png", rect.position + rect.size - glm::vec2(resize_icon_size * 0.5f, resize_icon_size * 0.5f), resize_icon_size);
 }
@@ -83,32 +83,43 @@ bool GuiResizableDialog::onMouseDown(sp::io::Pointer::Button button, glm::vec2 p
 {
     click_state = ClickState::None;
     if (title_bar->getRect().contains(position))
+    {
         click_state = ClickState::Drag;
+        drag_start_mouse = position;
+        drag_start_dialog = rect.position;
+    }
+
     if (!minimized && position.x >= rect.position.x + rect.size.x - resize_icon_size && position.y >= rect.position.y + rect.size.y - resize_icon_size)
         click_state = ClickState::Resize;
-    click_offset = position - glm::vec2(rect.position.x, rect.position.y);
+
+    click_offset = position - rect.position;
     return true;
 }
 
 void GuiResizableDialog::onMouseDrag(glm::vec2 position, sp::io::Pointer::ID id)
 {
-    glm::vec2 offset = position - rect.position;
-    switch(click_state)
+    switch (click_state)
     {
-    case ClickState::None:
-        break;
-    case ClickState::Drag:
-        setPosition(getPositionOffset() + offset - click_offset);
-        break;
-    case ClickState::Resize:
-        offset = offset - click_offset;
-        setSize(getSize() + offset);
-        click_offset += offset;
-        if (getSize().x < min_size.x)
-            setSize(min_size.x, getSize().y);
-        if (getSize().y < min_size.y)
-            setSize(getSize().x, min_size.y);
-        break;
+        case ClickState::None:
+            break;
+        case ClickState::Drag:
+        {
+            glm::vec2 new_position = drag_start_dialog + (position - drag_start_mouse);
+            setPosition(glm::vec2(std::clamp(new_position.x, -50.0f, window_size.x - 50.0f), std::clamp(new_position.y, 0.0f, window_size.y - title_bar_height)));
+            break;
+        }
+        case ClickState::Resize:
+        {
+            glm::vec2 offset = position - rect.position;
+            offset -= click_offset;
+            setSize(getSize() + offset);
+            click_offset += offset;
+            if (getSize().x < min_size.x)
+                setSize(min_size.x, getSize().y);
+            if (getSize().y < min_size.y)
+                setSize(getSize().x, min_size.y);
+            break;
+        }
     }
 }
 
