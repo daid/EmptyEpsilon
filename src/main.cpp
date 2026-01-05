cadacfa6b74fccdfc0bc8aa9e4636b9794b61efc
oznogon <oznogon@users.noreply.github.com>
2026-01-05 01:08:34 -0800

    Disable voice chat with voice_chat_enabled=0 Pref
    EE's built-in voice chat can degrade all Bluetooth audio output on
    some Linux distros while EE is running and a headset with mic is
    connected, even if EE's built-in voice isn't sending or receiving
    voice data. For example, Fedora 40+ drops audio output from A2DP
    to HSP/HFP while EE is running.
    
    Add a Preferences File option to disable built-in voice chat. This
    setting defaults to 1 (enabled) and should not change the existing
    default state of EE's built-in voice chat.

--- src/main.cpp
+++ src/main.cpp
@@ -172,9 +172,12 @@ int main(int argc, char** argv)
     // Since there is no way to access it (yet) via a touchscreen, compile out.
 #if !defined(ANDROID)
     // Set up voice chat and key bindings.
-    NetworkAudioRecorder* nar = new NetworkAudioRecorder();
-    nar->addKeyActivation(&keys.voice_all, 0);
-    nar->addKeyActivation(&keys.voice_ship, 1);
+    if (PreferencesManager::get("voice_chat_enabled", "1") == "1")
+    {
+        NetworkAudioRecorder* nar = new NetworkAudioRecorder();
+        nar->addKeyActivation(&keys.voice_all, 0);
+        nar->addKeyActivation(&keys.voice_ship, 1);
+    }
 #endif
 
     P<HardwareController> hardware_controller = new HardwareController();
