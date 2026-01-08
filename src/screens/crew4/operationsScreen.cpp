f0555edaabbedf160ab57d71bba83dc629066533
Garrett Guillotte <oznogon@users.noreply.github.com>
2026-01-08 01:15:20 -0800

    [#2667] Visually indicate Relay/Ops states (#2669)
    * Add Relay waypoint/probe state feedback
    
    - Add Cancel button to exit waypoint placement and probe launch
      states.
    - Add cursor state changes to indicate mouse usage.
    
    * Fix wayward pixel on create icon
    
    * Indicate Ops waypoint placement state using toggle button

--- src/screens/crew4/operationsScreen.cpp
+++ src/screens/crew4/operationsScreen.cpp
@@ -1,10 +1,11 @@
+#include "operationsScreen.h"
 #include "playerInfo.h"
 #include "i18n.h"
 #include "gameGlobalInfo.h"
-#include "operationsScreen.h"
 #include "preferenceManager.h"
 
 #include "gui/gui2_keyvaluedisplay.h"
+#include "gui/gui2_togglebutton.h"
 
 #include "components/scanning.h"
 #include "components/radar.h"
@@ -62,6 +63,7 @@ OperationScreen::OperationScreen(GuiContainer* owner)
                 if (my_spaceship)
                     my_player_info->commandAddWaypoint(position);
                 mode = TargetSelection;
+                place_waypoint_button->setValue(false);
                 break;
             case MoveWaypoint:
                 mode = TargetSelection;
@@ -84,7 +86,9 @@ OperationScreen::OperationScreen(GuiContainer* owner)
         ->setSize(200.0f, 50.0f);
 
     // Manage waypoints.
-    place_waypoint_button = new GuiButton(relay_functions, "WAYPOINT_PLACE_BUTTON", tr("Place waypoint"), [this]() { mode = WaypointPlacement; });
+    place_waypoint_button = new GuiToggleButton(relay_functions, "WAYPOINT_PLACE_BUTTON", tr("Place waypoint"), [this](bool value) {
+        mode = value ? WaypointPlacement : TargetSelection;
+    });
     place_waypoint_button->setSize(200.0f, 50.0f);
 
     delete_waypoint_button = new GuiButton(relay_functions, "WAYPOINT_DELETE_BUTTON", tr("Delete waypoint"),
