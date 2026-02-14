77241287427809831e97e4d4dec6c9b07dfa50db
oznogon <oznogon@users.noreply.github.com>
2026-02-13 21:20:47 -0800

    Remove hailed_player state from isCommsBeingHailed() check because it doesn't exist

--- scripts/api/entity/playerspaceship.lua
+++ scripts/api/entity/playerspaceship.lua
@@ -110,7 +110,7 @@ end
 function Entity:isCommsBeingHailed()
     if self.components.comms_transmitter then
         local state = self.components.comms_transmitter.state
-        return state == "hailed" or state == "hailed_player" or state == "hailed_gm"
+        return state == "hailed" or state == "hailed_gm"
     end
 end
 --- Returns whether this PlayerSpaceship is being hailed by the GM.
