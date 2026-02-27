f5d2679ae58740f2968f655efe8ac38777f2975d
oznogon <oznogon@users.noreply.github.com>
2026-02-26 19:09:51 -0800

    Add StdinLuaConsole history and commands
    - If running headless, add a log line after scenario launch to
      report that StdinLuaConsole is enabled.
    - Add bang commands to the headless mode StdinLuaConsole.
      - Add !help command to list bang commands.
      - Add !history command to list all entered Lua commands.
      - Add !save command to save Lua commands to a given file.
      - Add !clear command to clear the Lua command history.
      - Add !globals command to list all global values and functions in
        the active Lua environment.
      - Add !<N> command to execute a specifc history item.
      - Add !! command to execute the most recent history item.

--- src/stdinLuaConsole.h
+++ src/stdinLuaConsole.h
@@ -4,12 +4,17 @@
 #include "stringImproved.h"
 
 
-class StdinLuaConsole : public Updatable {
+class StdinLuaConsole : public Updatable
+{
 public:
+    StdinLuaConsole();
     void update(float delta) override;
 
 private:
     void addInput(char c);
+    void executeCommand(const string& cmd);
+    void printPrompt();
 
     string buffer;
+    std::vector<string> history;
 };
