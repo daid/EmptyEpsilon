38c6f1e12f6106d8c7b87433e4a14d49deae3a4f
Oznogon <garrett+ozgh@oznogon.com>
2026-03-03 18:06:00 -0800

    Refactor MouseRenderer to allow customization
    Add new functions to allow customizing the mouse cursor's
    appearance and behavior. No default behaviors should be changed.
    
    - Add MouseRenderer::setSpriteSize(), setSpriteColor(), and
      setClickPoint() to allow customizing the mouse cursor sprite.
    - Add convenience functions setClickPointCenter() and
      setClickPointTopLeft() to position the click point to the sprite
      center (default behavior) or the top-left corner.
    - Add drawLine visualization on Debug builds to show the sprite
      bounds and click point.

--- src/gui/mouseRenderer.cpp
+++ src/gui/mouseRenderer.cpp
@@ -4,19 +4,38 @@
 MouseRenderer::MouseRenderer(RenderLayer* render_layer)
 : Renderable(render_layer)
 {
-    visible = true;
+#ifdef DEBUG
+    show_bounds = true;
+#endif
 }
 
 void MouseRenderer::render(sp::RenderTarget& renderer)
 {
     if (!visible) return;
 
-    renderer.drawSprite(sprite, position, 32.0);
+    renderer.drawSprite(sprite, position + click_point, sprite_size, sprite_color);
+
+    if (show_bounds)
+    {
+        // Draw yellow square around cursor sprite.
+        const float sprite_bounds = sprite_size * 0.5f;
+        const glm::vec2 center = position + click_point;
+        const glm::vec2 top_left{center.x - sprite_bounds, center.y - sprite_bounds};
+        const glm::vec2 top_right{center.x + sprite_bounds, center.y - sprite_bounds};
+        const glm::vec2 bottom_right{center.x + sprite_bounds, center.y + sprite_bounds};
+        const glm::vec2 bottom_left{center.x - sprite_bounds, center.y + sprite_bounds};
+        renderer.drawLine({top_left, top_right, bottom_right, bottom_left, top_left}, {255, 255, 0, 200});
+
+        // Draw white crosshair on click point.
+        renderer.drawLine(position - glm::vec2{2.0f, 0.0f}, position + glm::vec2{2.0f, 0.0f}, {255, 255, 255, 255});
+        renderer.drawLine(position - glm::vec2{0.0f, 2.0f}, position + glm::vec2{0.0f, 2.0f}, {255, 255, 255, 255});
+    }
 }
 
 bool MouseRenderer::onPointerMove(glm::vec2 position, sp::io::Pointer::ID id)
 {
-    if (id == -1) {
+    if (id == -1)
+    {
         this->position = position;
         visible = true;
     }
@@ -25,14 +44,48 @@ bool MouseRenderer::onPointerMove(glm::vec2 position, sp::io::Pointer::ID id)
 
 void MouseRenderer::onPointerLeave(sp::io::Pointer::ID id)
 {
-    if (id == -1)
-        visible = false;
+    if (id == -1) visible = false;
 }
 
 void MouseRenderer::onPointerDrag(glm::vec2 position, sp::io::Pointer::ID id)
 {
-    if (id == -1) {
-        this->position = position;
-        visible = true;
-    }
+    onPointerMove(position, id);
+}
+
+void MouseRenderer::setSpriteImage(string sprite_image)
+{
+    sprite = sprite_image;
+}
+
+void MouseRenderer::setSpriteSize(float size)
+{
+    // Render the sprite no smaller than 2x2.
+    const float new_size = std::max(2.0f, size);
+
+    // Set the click point in the new size to the same position relative to its
+    // current size.
+    click_point = click_point * (new_size / sprite_size);
+    sprite_size = new_size;
+}
+
+void MouseRenderer::setSpriteColor(glm::u8vec4 color)
+{
+    sprite_color = color;
+}
+
+void MouseRenderer::setClickPoint(glm::vec2 point)
+{
+    const float sprite_bounds = sprite_size * 0.5f;
+    click_point = glm::clamp(point, -sprite_bounds, sprite_bounds);
+}
+
+void MouseRenderer::setClickPointCenter()
+{
+    click_point = glm::vec2{0.0f, 0.0f};
+}
+
+void MouseRenderer::setClickPointTopLeft()
+{
+    const float sprite_bounds = sprite_size * 0.5f;
+    click_point = glm::vec2{sprite_bounds, sprite_bounds};
 }
