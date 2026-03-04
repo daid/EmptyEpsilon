d5bf8973c1f3d7cbb844b7379fda3c2391d897f9
Dave Kapell <david@rigitech.com>
2026-03-04 09:42:55 -0500

    Create Post Processor Component and System

--- src/multiplayer/player.cpp
+++ src/multiplayer/player.cpp
@@ -12,8 +12,15 @@ BASIC_REPLICATION_IMPL(PlayerControlReplication, PlayerControl)
     BASIC_REPLICATION_FIELD(main_screen_overlay);
     BASIC_REPLICATION_FIELD(alert_level);
 
-    BASIC_REPLICATION_FIELD(control_code); //TODO: Instead of replicating this to clients, check it on receiving the commandSetShip in playerinfo
+    BASIC_REPLICATION_FIELD(control_code); //TODO: Indstead of replicating this to clients, check it on receiving the commandSetShip in playerinfo
     BASIC_REPLICATION_FIELD(allowed_positions.mask);
+    BASIC_REPLICATION_FIELD(in_gravity);
+    BASIC_REPLICATION_FIELD(glitch_alpha);
+    BASIC_REPLICATION_FIELD(glitch_alpha_decay_rate);
+    BASIC_REPLICATION_FIELD(warp_alpha_decay_rate);
+    BASIC_REPLICATION_FIELD(warp_alpha);
+
+    BASIC_REPLICATION_FIELD(just_teleported);
 }
 
 BASIC_REPLICATION_IMPL(WaypointsReplication, Waypoints)
