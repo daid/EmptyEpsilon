f4d3c76ffbb29454e4c2cba75184feb253859cf7
Dave Kapell <david@rigitech.com>
2026-03-04 14:51:20 -0500

    Update player.cpp

--- src/multiplayer/player.cpp
+++ src/multiplayer/player.cpp
@@ -12,7 +12,7 @@ BASIC_REPLICATION_IMPL(PlayerControlReplication, PlayerControl)
     BASIC_REPLICATION_FIELD(main_screen_overlay);
     BASIC_REPLICATION_FIELD(alert_level);
 
-    BASIC_REPLICATION_FIELD(control_code); //TODO: Indstead of replicating this to clients, check it on receiving the commandSetShip in playerinfo
+    BASIC_REPLICATION_FIELD(control_code); //TODO: Instead of replicating this to clients, check it on receiving the commandSetShip in playerinfo
     BASIC_REPLICATION_FIELD(allowed_positions.mask);
     BASIC_REPLICATION_FIELD(in_gravity);
     BASIC_REPLICATION_FIELD(glitch_alpha);
