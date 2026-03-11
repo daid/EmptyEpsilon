4c87f9836329ff0a96a5f88c47f0012729be7548
Garrett Guillotte <oznogon@users.noreply.github.com>
2026-03-11 04:30:55 -0700

    Allow zooming GM, Spectator views out to 10kU view distance (#2502)
    * Reduce sector drawing while zooming out
    
    When zooming out sufficiently far, sectors become
    unreadable and sector grid drawing begins to
    degrade performance.
    
    When zooming out past a distance 8x the sector
    size (160k), draw squares the size of 8x8 sectors
    instead of 1x1 on the grid and stop drawing labels.
    
    * Allow GM screen to zoom out to 1mil distance
    
    * Allow spectator screen to zoom out ot 1mil distances

--- src/screens/gm/gameMasterScreen.cpp
+++ src/screens/gm/gameMasterScreen.cpp
@@ -324,7 +324,7 @@ void GameMasterScreen::update(float delta)
     float mouse_wheel_delta = keys.zoom_in.getValue() - keys.zoom_out.getValue();
     if (mouse_wheel_delta != 0.0f)
     {
-        float view_distance = std::clamp(main_radar->getDistance() * (1.0f - (mouse_wheel_delta * 0.1f)), 5000.0f, 100000.0f);
+        float view_distance = std::clamp(main_radar->getDistance() * (1.0f - (mouse_wheel_delta * 0.1f)), 5000.0f, 1000000.0f);
         main_radar->setDistance(view_distance);
         if (view_distance < 10000) main_radar->shortRange();
         else main_radar->longRange();
