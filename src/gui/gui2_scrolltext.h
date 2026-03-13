345d7c935b4c4bd8222529548333f1684b75c720
Oznogon <garrett+ozgh@oznogon.com>
2026-03-13 14:09:38 -0700

    Refactor GuiScrollText, GuiScrollFormattedText
    Refactor scrolling text elements to use GuiScrollContainer and
    GuiMultilineText. This replaces the bespoke scrolling and clipping
    behaviors with those of GuiScrollContainer, and works around font
    preparation sizing issues with descenders by building in a buffer
    for them.
    
    While this reimplementation has the same interface as the prior
    GuiScrollText elements, its reparenting to GuiScrollContainer forces
    a change to method chaining order in the GM chat dialog.

--- src/gui/gui2_scrolltext.h
+++ src/gui/gui2_scrolltext.h
@@ -1,38 +1,48 @@
 #pragma once
 
-#include "gui2_element.h"
-#include "gui2_scrollbar.h"
+#include "gui2_scrollcontainer.h"
+#include "gui2_multilinetext.h"
 
-class GuiScrollText : public GuiElement
+// A GuiScrollContainer wrapper for GuiMultilineText that maintains backward
+// compatibility with legacy GuiScrollText.
+class GuiScrollText : public GuiScrollContainer
 {
 protected:
-    GuiScrollbar* scrollbar;
+    // The text to render. Passed directly to GuiMultilineText.
     string text;
+    // Base font size, in virtual pixels. Passed directly to GuiMultilineText.
     float text_size = 30.0f;
+    // Determines whether to automatically scroll text to the bottom when the
+    // text changes.
     bool auto_scroll_down = false;
+    // Sets the mouse scroll interval as the number of scrollbar steps from top
+    // to bottom. (Not implemented yet)
     int mouse_scroll_steps = 25;
+    // The multiline text element. GuiScrollFormattedText overwrites this with a
+    // GuiMultilineFormattedText element.
+    GuiMultilineText* text_element;
 
 public:
     GuiScrollText(GuiContainer* owner, string id, string text);
 
+    // Enables automatic scrolling to the bottom when the text changes.
     GuiScrollText* enableAutoScrollDown() { auto_scroll_down = true; return this; }
+    // Disables automatic scrolling to the bottom when the text changes.
     GuiScrollText* disableAutoScrollDown() { auto_scroll_down = false; return this; }
 
+    // Sets the element's text contents. Use control characters like \n to add
+    // line breaks. If automatic scrolling is enabled, this triggers it.
     GuiScrollText* setText(string text);
-    string getText() const;
-    GuiScrollText* setTextSize(float text_size) { this->text_size = text_size; return this; }
-
-    GuiScrollText* setScrollbarWidth(float width);
-
-    virtual void onDraw(sp::RenderTarget& renderer) override;
-    virtual bool onMouseWheelScroll(glm::vec2 position, float value) override;
+    // Returns the element's text.
+    string getText() const { return text; }
+    // Sets the font size to a value of at least 1px.
+    GuiScrollText* setTextSize(float text_size) { text_element->setTextSize(text_size); return this; }
 };
 
+// A GuiScrollText wrapper for GuiMultilineFormattedText, reusing everything but
+// text_element.
 class GuiScrollFormattedText : public GuiScrollText
 {
 public:
     GuiScrollFormattedText(GuiContainer* owner, string id, string text);
-
-    virtual void onDraw(sp::RenderTarget& renderer) override;
-    virtual bool onMouseWheelScroll(glm::vec2 position, float value) override;
 };
