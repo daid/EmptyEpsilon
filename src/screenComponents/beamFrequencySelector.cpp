39b3be8b61676467ff318718c4d8470d56d7cab9
oznogon <oznogon@users.noreply.github.com>
2026-03-14 11:12:28 -0700

    Update BeamFrequencySelector if value changes
    Update the selected index on BeamFrequencySelector when the beam
    frequency changes elsewhere, for instance on another crew screen or
    via Lua.

--- src/screenComponents/beamFrequencySelector.cpp
+++ src/screenComponents/beamFrequencySelector.cpp
@@ -1,41 +1,43 @@
+#include "beamFrequencySelector.h"
 #include "playerInfo.h"
 #include "gameGlobalInfo.h"
-#include "beamFrequencySelector.h"
 
 #include "components/beamweapon.h"
 
 GuiBeamFrequencySelector::GuiBeamFrequencySelector(GuiContainer* owner, string id)
 : GuiSelector(owner, id, [](int index, string value) { if (my_spaceship) my_player_info->commandSetBeamFrequency(index); })
 {
-    for(int n=0; n<=BeamWeaponSys::max_frequency; n++)
+    for (int n = 0; n <= BeamWeaponSys::max_frequency; n++)
         addEntry(frequencyToString(n), frequencyToString(n));
-    if (my_spaceship) {
-        auto beamweapons = my_spaceship.getComponent<BeamWeaponSys>();
-        if (beamweapons)
-            setSelectionIndex(beamweapons->frequency);
-    }
-    if (!gameGlobalInfo->use_beam_shield_frequencies)
-        hide();
 }
 
 void GuiBeamFrequencySelector::onUpdate()
 {
-    if (my_spaceship && gameGlobalInfo->use_beam_shield_frequencies && isVisible())
+    if (!my_spaceship) return;
+    auto beam_weapons = my_spaceship.getComponent<BeamWeaponSys>();
+    setVisible(beam_weapons && gameGlobalInfo->use_beam_shield_frequencies);
+    if (!isVisible()) return;
+
+    // Handle inc/dec keybinds.
+    if (keys.weapons_beam_frequence_increase.getDown())
     {
-        if (keys.weapons_beam_frequence_increase.getDown())
-        {
-            if (getSelectionIndex() >= (int)entries.size() - 1)
-                setSelectionIndex(0);
-            else
-                setSelectionIndex(getSelectionIndex() + 1);
-            callback();
-        }
-        if (keys.weapons_beam_frequence_decrease.getDown())
-        {
-            if (getSelectionIndex() <= 0)
-                setSelectionIndex(entries.size() - 1);
-            else
-                setSelectionIndex(getSelectionIndex() - 1);
-        }
+        if (getSelectionIndex() >= static_cast<int>(entries.size()) - 1)
+            setSelectionIndex(0);
+        else
+            setSelectionIndex(getSelectionIndex() + 1);
+
+        callback();
     }
+
+    if (keys.weapons_beam_frequence_decrease.getDown())
+    {
+        if (getSelectionIndex() <= 0)
+            setSelectionIndex(static_cast<int>(entries.size()) - 1);
+        else
+            setSelectionIndex(getSelectionIndex() - 1);
+    }
+
+    // Sync selector to current beam frequency.
+    if (beam_weapons->frequency != getSelectionIndex())
+        setSelectionIndex(beam_weapons->frequency);
 }
