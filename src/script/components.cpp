3a44b986cac71cd89868cff3c034ed29c3fc8b67
Garrett Guillotte <oznogon@users.noreply.github.com>
2026-03-28 01:24:35 -0700

    [#2776] Fix AvoidObject range edge cases (#2816)
    * Clamp AvoidObject range, don't subtract range from startEndLength
    
    * Use getter/setter to validate AvoidObject ranges

--- src/script/components.cpp
+++ src/script/components.cpp
@@ -297,11 +297,11 @@ void initComponentScriptBindings()
     BIND_MEMBER(Orbit, time);
 
     sp::script::ComponentHandler<AvoidObject>::name("avoid_object");
-    BIND_MEMBER(AvoidObject, range);
+    BIND_MEMBER_GS(AvoidObject, "range", getRange, setRange);
 
     sp::script::ComponentHandler<DelayedAvoidObject>::name("delayed_avoid_object");
     BIND_MEMBER(DelayedAvoidObject, delay);
-    BIND_MEMBER(DelayedAvoidObject, range);
+    BIND_MEMBER_GS(DelayedAvoidObject, "range", getRange, setRange);
 
     sp::script::ComponentHandler<ExplodeOnTouch>::name("explode_on_touch");
     BIND_MEMBER(ExplodeOnTouch, damage_at_center);
