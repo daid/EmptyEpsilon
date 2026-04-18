795130724476361da8f2436a6265b4080b6cfa41
oznogon <oznogon@users.noreply.github.com>
2026-04-18 16:38:22 -0700

    Fix waypoint route replication

--- src/multiplayer/player.cpp
+++ src/multiplayer/player.cpp
@@ -2,8 +2,24 @@
 #include "multiplayer.h"
 
 namespace sp::io {
-    static inline DataBuffer& operator << (DataBuffer& packet, const Waypoints::Point& p) { return packet << p.id << p.position; }
-    static inline DataBuffer& operator >> (DataBuffer& packet, Waypoints::Point& p) { packet >> p.id >> p.position; return packet; }
+    static inline DataBuffer& operator << (DataBuffer& packet, const Waypoints::Point& p) { return packet << p.id << p.set_id << p.position; }
+    static inline DataBuffer& operator >> (DataBuffer& packet, Waypoints::Point& p) { packet >> p.id >> p.set_id >> p.position; return packet; }
+
+    static inline DataBuffer& operator << (DataBuffer& packet, const std::array<bool, Waypoints::MAX_SETS>& arr)
+    {
+        uint8_t bits = 0;
+        for (int i = 0; i < Waypoints::MAX_SETS; i++)
+            if (arr[i]) bits |= (1 << i);
+        return packet << bits;
+    }
+    static inline DataBuffer& operator >> (DataBuffer& packet, std::array<bool, Waypoints::MAX_SETS>& arr)
+    {
+        uint8_t bits;
+        packet >> bits;
+        for (int i = 0; i < Waypoints::MAX_SETS; i++)
+            arr[i] = (bits >> i) & 1;
+        return packet;
+    }
 }
 
 
@@ -18,4 +34,5 @@ BASIC_REPLICATION_IMPL(PlayerControlReplication, PlayerControl)
 
 BASIC_REPLICATION_IMPL(WaypointsReplication, Waypoints)
     REPLICATE_VECTOR_IF_DIRTY(waypoints, dirty);
+    BASIC_REPLICATION_FIELD(is_route);
 }
